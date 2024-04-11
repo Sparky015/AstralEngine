721adb6158351ab49b9701da021f72c3a2df0d1b
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2024-04-10 22:34:24 -0400

    Add build files to make it run

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
 
+
     SDL_CreateWindow("test", 100, 100, 500, 500, SDL_WINDOW_SHOWN);
     SDL_Event e; bool quit = false; while( quit == false ){ while( SDL_PollEvent( &e ) ){ if( e.type == SDL_QUIT ) quit = true; } }
     return 0;
