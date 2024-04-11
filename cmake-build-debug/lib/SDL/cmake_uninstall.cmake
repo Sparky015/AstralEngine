0425f8b676e8a8a0d2b2304ae8240b616ca0cfc4
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2024-04-10 22:23:40 -0400

    trying to get the build directory to the git

--- cmake-build-debug/lib/SDL/cmake_uninstall.cmake
+++ cmake-build-debug/lib/SDL/cmake_uninstall.cmake
@@ -7,7 +7,7 @@ string(REGEX REPLACE "\n" ";" files "${files}")
 foreach (file ${files})
     message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
     execute_process(
-        COMMAND /Users/sparky/Applications/CLion 2024.1 Release Candidate.app/Contents/bin/cmake/mac/aarch64/bin/cmake -E remove "$ENV{DESTDIR}${file}"
+        COMMAND /Users/sparky/Applications/CLion Nova.app/Contents/bin/cmake/mac/aarch64/bin/cmake -E remove "$ENV{DESTDIR}${file}"
         OUTPUT_VARIABLE rm_out
         RESULT_VARIABLE rm_retval
     )
