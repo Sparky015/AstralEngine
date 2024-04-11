1a72d973b146f13626532060875e11f2c0fa4199
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2024-04-10 22:32:33 -0400

    Add build files to make it run

--- cmake-build-debug/lib/SDL/include/SDL2/SDL_revision.h
+++ cmake-build-debug/lib/SDL/include/SDL2/SDL_revision.h
@@ -2,7 +2,7 @@
 #define SDL_REVISION_NUMBER 0
 
 #ifdef SDL_VENDOR_INFO
-#define SDL_REVISION "SDL-2.30.2-g566c1c7 (" SDL_VENDOR_INFO ")"
+#define SDL_REVISION "SDL-2.30.2-g0425f8b (" SDL_VENDOR_INFO ")"
 #else
-#define SDL_REVISION "SDL-2.30.2-g566c1c7"
+#define SDL_REVISION "SDL-2.30.2-g0425f8b"
 #endif
