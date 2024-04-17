50d84141ab9ea8fb4523c27af3fddcc9d4975054
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2024-04-16 20:24:42 -0400

    Update library linking and switch from SDL to GLFW
    Updated the way libraries are linked in CMakeLists.txt to support both Apple and Windows platforms. Refactored the entry point in EngineLoop.cpp, switching from SDL to GLFW for window creation and event polling.

--- src/Ayla/EntryPoint/EngineLoop.cpp
+++ src/Ayla/EntryPoint/EngineLoop.cpp
@@ -8,28 +8,48 @@
 #include <SDL.h>
 #include <cstdlib>
 
+#include <GLFW/glfw3.h>
 
 namespace Ayla::EntryPoint {
 
     void EngineLoop(){
         std::cout << "Hello, World!" << std::endl;
 
-        // Remember to initialize SDL
-        if (SDL_Init(SDL_INIT_VIDEO) < 0){
-            std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+//        // Remember to initialize SDL
+//        if (SDL_Init(SDL_INIT_VIDEO) < 0){
+//            std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
+//            exit(EXIT_FAILURE);
+//        }
+//
+//        SDL_CreateWindow("test", 100, 100, 500, 500, SDL_WINDOW_SHOWN);
+//        SDL_Event event;
+//        bool hasQuit = false;
+//        while(!hasQuit){
+//            while(SDL_PollEvent( &event )){
+//                if(event.type == SDL_QUIT) { hasQuit = true; }
+//            }
+//        }
+//
+//        SDL_Quit();   // Cleanup after we're done
+
+        if (!glfwInit()) {
+            std::cout << "GLFW could not initialize!" << std::endl;
             exit(EXIT_FAILURE);
         }
 
-        SDL_CreateWindow("test", 100, 100, 500, 500, SDL_WINDOW_SHOWN);
-        SDL_Event event;
-        bool hasQuit = false;
-        while(!hasQuit){
-            while(SDL_PollEvent( &event )){
-                if(event.type == SDL_QUIT) { hasQuit = true; }
-            }
+        GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL Window", nullptr, nullptr);
+        if (!window) {
+            std::cout << "Failed to create GLFW window" << std::endl;
+            glfwTerminate();
+            exit(EXIT_FAILURE);
         }
 
-        SDL_Quit();   // Cleanup after we're done
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
+
+        glfwTerminate();
+
 
         exit(EXIT_SUCCESS);
     }
