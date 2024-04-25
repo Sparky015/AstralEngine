b3a48f47828035ae678ff3c150bb0f8e5de1f32c
sparky <m@>
2024-04-24 20:07:05 -0400

    Remove generated Doxygen files

--- docs/Doxygen/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['ayla_0',['Ayla',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- docs/Doxygen/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['progress_0',['Work in Progress.',['../md__r_e_a_d_m_e.html#autotoc_md1',1,'']]]
-];
