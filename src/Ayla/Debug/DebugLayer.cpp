c88b3f2db17824734b76dc671e1559d53f7153f5
sparky <m@>
2024-07-04 20:44:28 -0400

    - Added new DebugLayer class in Debug directory - Implemented DebugLayer functionality (attach, detach, update, etc.)
    In this commit, a new class, DebugLayer, has been introduced in the Debug directory. This class includes basic layer operations such as attach, detach, and update plus additional functionality specific to debugging. Providing a robust framework for debugging will improve the development process, making it easier to clean and optimize the code.

---
+++ src/Ayla/Debug/DebugLayer.cpp
@@ -0,0 +1,69 @@
+//
+// Created by Andrew Fagan on 6/25/24.
+//
+
+#include "DebugLayer.h"
+#include "Ayla/Core/Application.h"
+#include "Ayla/Input/InputState.h"
+#include "Ayla/Input/Keycodes.h"
+#include "Ayla/Core/Time/Time.h"
+
+using namespace Ayla::Input;
+
+namespace Ayla::Core::Debug {
+
+
+
+    DebugLayer::DebugLayer(){
+        AY_TRACE("[Sholas] DebugLayer: Initializing Debug Layer");
+        this->attachLayer();
+        m_debugName = "Debug Layer";
+    }
+
+    DebugLayer::~DebugLayer(){
+        AY_TRACE("[Sholas] DebugLayer: Destroying Debug Layer");
+        this->detachLayer();
+    }
+
+    void DebugLayer::onAttach() {
+        m_isEnabled = true;
+    }
+
+    void DebugLayer::onDetach() {
+        m_isEnabled = false;
+    }
+
+    void DebugLayer::onUpdate() {
+        static long long accumulatedTime;
+        accumulatedTime += Time::Clock::get().getDeltaTime();
+        if (InputState::get().isKeyDown(AY_KEY_T) && !InputState::get().isKeyRepeating(AY_KEY_T)){
+            if (accumulatedTime <= 75){return;}
+            accumulatedTime = 0;
+            AY_TRACE("T is pressed down.");
+            std::cout << "\n" << InputState::get().mousePositionX() << " by " << InputState::get().mousePositionY();
+        }
+        if (InputState::get().isKeyDown(AY_KEY_R) && !InputState::get().isKeyRepeating(AY_KEY_R)){
+            if (accumulatedTime <= 300){return;}
+            accumulatedTime = 0;
+            AY_TRACE("Resetting stopwatch.");
+            Ayla::Core::Time::Clock::get().resetStopwatch();
+        }
+    }
+
+    void DebugLayer::onEvent(Event& event) {
+        // Not accepting events
+    }
+
+    EventCategory DebugLayer::getAcceptingEventFlags() {
+        return None;
+    }
+
+    void DebugLayer::onImGuiRender() {
+
+    }
+
+}
+
+
+
+
---
+++ src/Ayla/Debug/DebugLayer.h
@@ -0,0 +1,28 @@
+//
+// Created by Andrew Fagan on 6/25/24.
+//
+#pragma once
+
+#include "Ayla/Layers/Layer.h"
+
+using namespace Ayla::Core::Layers;
+
+namespace Ayla::Core::Debug {
+
+    class DebugLayer : public Layer {
+    public:
+        DebugLayer();
+        ~DebugLayer() override;
+
+        void onAttach() override;
+        void onDetach() override;
+        void onUpdate() override;
+        void onImGuiRender() override;
+        void onEvent(Event&) override;
+        EventCategory getAcceptingEventFlags() override;
+
+    private:
+
+    };
+
+}
