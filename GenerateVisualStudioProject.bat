a42cd11787747742e9eade2b7f62a4e0ad62ef8e
sparky <m@>
2024-07-16 22:07:13 -0400

    - Added .vs to .gitignore - Updated comment syntax in GenerateVisualStudioProject.bat
    In this commit, we've updated the .gitignore file to ignore the .vs directory which is typically generated during Visual Studio projects. Moreover, we've also improved the readability of GenerateVisualStudioProject.bat by updating the comment syntax for consistency across all scripts.

--- GenerateVisualStudioProject.bat
+++ GenerateVisualStudioProject.bat
@@ -1,4 +1,4 @@
-# Must be used from the root of the project.
+REM Must be used from the root of the project.
 mkdir build-visual-studio
 cd build-visual-studio || exit
 cmake -G "Visual Studio 17 2022" -A x64 ..
