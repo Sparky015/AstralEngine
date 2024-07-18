3357524b25859f844f5e4f184cc263b0826abc97
sparky <m@>
2024-07-17 21:16:03 -0400

    Video 4: Writing a Shader in OpenGL
    - Wrote a vertex and fragment shader

--- source/Ayla/Renderer/RendererLayer.cpp
+++ source/Ayla/Renderer/RendererLayer.cpp
@@ -7,54 +7,107 @@
 #include "glad/glad.h"
 
 
-Ayla::Renderer::RendererLayer::RendererLayer() :m_Buffer(0)
-{
-    AY_TRACE("[Sholas] RendererLayer: Initializing Renderer Layer");
-    this->AttachLayer();
-    m_DebugName = "Renderer Layer";
-}
+namespace Ayla::Renderer {
 
 
-Ayla::Renderer::RendererLayer::~RendererLayer()
-{
-    AY_TRACE("[Sholas] RendererLayer: Destroying Renderer Layer");
-    OnDetach();
-    this->DetachLayer();
-}
+    RendererLayer::RendererLayer() : m_Buffer(0)
+    {
+        AY_TRACE("[Sholas] RendererLayer: Initializing Renderer Layer");
+        this->AttachLayer();
+        m_DebugName = "Renderer Layer";
+    }
 
 
-void Ayla::Renderer::RendererLayer::OnAttach()
-{
-    float positions[6] = {
-        -0.5f, -0.5,
-        0.0f, 0.5f,
-        0.5f, -0.5f
-    };
-
-    glGenBuffers(1, &m_Buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, m_Buffer);
-
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
-    glEnableVertexAttribArray(0);
-
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), (float*)positions, GL_STATIC_DRAW);
-}
+    RendererLayer::~RendererLayer()
+    {
+        AY_TRACE("[Sholas] RendererLayer: Destroying Renderer Layer");
+        OnDetach();
+        this->DetachLayer();
+    }
 
 
-void Ayla::Renderer::RendererLayer::OnUpdate()
-{
-    glClearColor(1, 0, 1, 1);
-    glClear(GL_COLOR_BUFFER_BIT);
+    void RendererLayer::OnAttach()
+    {
+        float positions[6] = {
+            -0.5f, -0.5,
+            0.0f, 0.5f,
+            0.5f, -0.5f
+        };
+
+        glGenBuffers(1, &m_Buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, m_Buffer);
+
+        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+        glEnableVertexAttribArray(0);
+
+        glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), (float*)positions, GL_STATIC_DRAW);
+
+        unsigned int shader = CreateShader(m_VertexShader, m_FragmentShader);
+        glUseProgram(shader);
+    }
 
 
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-}
+    void RendererLayer::OnUpdate()
+    {
+        glClearColor(1, 0, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
 
 
-Ayla::Events::EEventCategory Ayla::Renderer::RendererLayer::GetAcceptingEventFlags()
-{
-    return Ayla::Events::None;
-}
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+
+    Events::EEventCategory RendererLayer::GetAcceptingEventFlags()
+    {
+        return Ayla::Events::None;
+    }
+
+
+    unsigned int RendererLayer::CompileShader(unsigned int type, const std::string& source)
+    {
+        unsigned int id = glCreateShader(type);
+        const char* src = source.c_str();
+        glShaderSource(id, 1, &src, nullptr);
+        glCompileShader(id);
+
+        int result;
+        glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+        if (result == GL_FALSE)
+        {
+            int length;
+            glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+            char* message = (char*)alloca(length * sizeof(char));
+            glGetShaderInfoLog(id, length, &length, message);
+            AY_LOG_SS("Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex!" : "fragment!"));
+            AY_LOG(message);
+            glDeleteShader(id);
+            return 0;
+        }
+
+        return id;
+    }
+
+
+    unsigned int RendererLayer::CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+    {
+        unsigned int program = glCreateProgram();
+        unsigned int vs_id = CompileShader(GL_VERTEX_SHADER, vertexShader);
+        unsigned int fs_id = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+        glAttachShader(program, vs_id);
+        glAttachShader(program, fs_id);
+
+        glLinkProgram(program);
+        glValidateProgram(program);
+
+        glDeleteShader(vs_id);
+        glDeleteShader(fs_id);
+
+        return program;
+    }
+
+
+} // namespace Ayla::Renderer
 
 
 
--- source/Ayla/Renderer/RendererLayer.h
+++ source/Ayla/Renderer/RendererLayer.h
@@ -18,10 +18,32 @@ public:
 
     virtual void OnAttach() override;
     virtual void OnUpdate() override;
-    virtual Events::EEventCategory GetAcceptingEventFlags();
+    virtual Events::EEventCategory GetAcceptingEventFlags() override;
+
+    static unsigned int CompileShader(unsigned int type, const std::string& source);
+    static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+
 private:
 
     unsigned int m_Buffer;
+
+    std::string m_VertexShader =
+        "#version 410\n"
+        "layout(location = 0) in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "gl_Position = position;\n"
+        "}\n"
+        "\n";
+
+    std::string m_FragmentShader =
+        "#version 410\n"
+        "layout(location = 0) out vec4 color;\n"
+        "void main()\n"
+        "{\n"
+        "color = vec4(0.0, 1.0, 0.0, 1.0);\n"
+        "}\n"
+        "\n";
 
 };
 
