dd1d4a990ceeda0aaf19f591de6cdc61928bdc36
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2024-11-16 22:23:39 -0500

    Enabled log files

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,14 +6,14 @@
 
 int main()
 {
+    Debug::Macros::InitLogFileForMacros();
     LOG("\n---   Entering main()    ---     (static and global object initialization above)\n");
-
     {
         Engine engine = Engine();
-//        Debug::LogEngineDebugInfo(); // Need to set up window and rendering context before calling this.
+        Debug::LogEngineDebugInfo(); // Need to set up window and rendering context before calling this.
         engine.Run();
     }
-
     LOG("\n---   Exiting main() ---     (static and global object destruction below)\n");
+    Debug::Macros::CloseLogFileForMacros();
     return 0;
 }
