2e6c08185585ce0750c0c7f28e96bc842341db32
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-01-03 02:22:02 -0500

    Add unlikely attributes to the ASSERT macros

--- Source/AstralEngine/Debug/Macros/Asserts.h
+++ Source/AstralEngine/Debug/Macros/Asserts.h
@@ -7,8 +7,8 @@
 #ifndef TURN_OFF_DEBUG_MACROS
 
 /** Asserts a conditional. Throws an error if the conditional isn't true and outputs a message to console. */
-#define ASSERT(expression, errorMessage)  if (!(expression)) {Debug::Macros::macro_ASSERT(#expression, errorMessage, __FILE__, __LINE__, __func__); throw std::runtime_error(errorMessage);}
-#define ASSERT_SS(expression, errorMessage) if (!(expression)) {std::ostringstream ss; ss << (errorMessage); Debug::Macros::macro_ASSERT(#expression ss, __FILE__, __LINE__, __func__); throw std::runtime_error(ss.str());}
+#define ASSERT(expression, errorMessage) [[unlikely]] if (!(expression)) {Debug::Macros::macro_ASSERT(#expression, errorMessage, __FILE__, __LINE__, __func__); throw std::runtime_error(errorMessage);}
+#define ASSERT_SS(expression, errorMessage) [[unlikely]] if (!(expression)) {std::ostringstream ss; ss << (errorMessage); Debug::Macros::macro_ASSERT(#expression, ss, __FILE__, __LINE__, __func__); throw std::runtime_error(ss.str());}
 #define STATIC_ASSERT(expression, errorMessage) static_assert(expression, errorMessage);
 
 #else
