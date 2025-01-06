7b28d542dafbb803ba8e40b6dd5665bcdf6aa2cf
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-01-06 17:36:22 -0500

    Removed cpuinfo

--- Source/AstralEngine/main.cpp
+++ Source/AstralEngine/main.cpp
@@ -3,15 +3,13 @@
 #include "Debug/Instrumentation/FunctionProfiler.h"
 
 //#include "Debug/AllocationTracker.h"
-#include "cpuinfo.h"
 
 int main()
 {
     LOG("\n---   Entering main()    ---     (static and global object initialization above)\n");
 
     LOG("Compiled On: " << __DATE__ << " " << __TIME__ << "\n");
-    cpuinfo_initialize();
-    LOG(cpuinfo_get_package(0)->name);
+
     {
         Engine engine = Engine();
         Debug::LogEngineDebugInfo(); // Need to set up window and rendering context before calling this.
