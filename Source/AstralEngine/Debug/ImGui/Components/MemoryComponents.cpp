f826aafc2206b1c1ac859a5e1964af9eece6f24b
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-01-10 17:52:55 -0500

    Added documentation for memory unit label helper

--- Source/AstralEngine/Debug/ImGui/Components/MemoryComponents.cpp
+++ Source/AstralEngine/Debug/ImGui/Components/MemoryComponents.cpp
@@ -10,57 +10,67 @@
 
 namespace Debug {
 
-    std::string_view MemorySizeLabelHelper(size_t numberOfBytes)
+    /**@brief Dynamically adjusts the unit label of the memory used based on the number of bytes being represented.
+     * @param numberOfBytes The number of bytes that are being represented
+     * @return A string representation of the memory size with the corresponding unit label
+     * @note The unit label only goes up to TB
+     * @warning The returned string_view is valid only until the next call to this function from the same thread.
+     *          Do not store or cache the returned string_view. */
+    std::string_view MemoryUnitLabelHelper(size_t numberOfBytes)
     {
         static constexpr int BUFFER_SIZE = 20;
+        static constexpr size_t KB = 1000;
+        static constexpr size_t MB = KB * 1000;
+        static constexpr size_t GB = MB * 1000;
+        static constexpr size_t TB = GB * 1000;
         thread_local char numberOfBytesString[BUFFER_SIZE] = {};
-        memset(numberOfBytesString, 0, BUFFER_SIZE);
 
-        if (numberOfBytes < 1000) // show bytes
+        if (numberOfBytes < KB) // show bytes
         {
             snprintf(numberOfBytesString, BUFFER_SIZE, "%zu bytes", numberOfBytes);
-            return numberOfBytesString;
         }
-        else if (numberOfBytes < 1000000) // show kilobytes
+        else if (numberOfBytes < MB) // show kilobytes
         {
-            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f KB", numberOfBytes / 1000.0f);
-            return numberOfBytesString;
+            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f KB", numberOfBytes / static_cast<float>(KB));
         }
-        else if (numberOfBytes < 1000000000) // show megabytes
+        else if (numberOfBytes < GB) // show megabytes
         {
-            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f MB", numberOfBytes / 1000000.0f);
-            return numberOfBytesString;
+            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f MB", numberOfBytes / static_cast<float>(MB));
         }
-        else // show gigabytes
+        else if (numberOfBytes < TB) // show gigabytes
         {
-            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f GB", numberOfBytes / 1000000.0f);
-            return numberOfBytesString;
+            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f GB", numberOfBytes / static_cast<float>(GB));
+        }
+        else // show terabytes
+        {
+            snprintf(numberOfBytesString, BUFFER_SIZE, "%.2f TB", numberOfBytes / static_cast<float>(TB));
         }
 
+        return numberOfBytesString;
     }
 
     void GlobalTotalAllocationsAndFrees()
     {
-        Core::MemoryMetricsManager& memoryMetricsManager = Core::MemoryMetricsManager::Get();
+        const Core::MemoryMetricsManager& memoryMetricsManager = Core::MemoryMetricsManager::Get();
         ImGui::Text("Total Allocated Bytes: %llu", memoryMetricsManager.GetTotalAllocatedBytes());
         ImGui::Text("Total Freed Bytes: %llu", memoryMetricsManager.GetTotalFreedBytes());
     }
 
     void GlobalMemoryUsage()
     {
-        Core::MemoryMetricsManager& memoryMetricsManager = Core::MemoryMetricsManager::Get();
-        ImGui::Text("Memory Usage: %s", MemorySizeLabelHelper(memoryMetricsManager.GetTotalAllocatedBytes() - memoryMetricsManager.GetTotalFreedBytes()).data());
+        const Core::MemoryMetricsManager& memoryMetricsManager = Core::MemoryMetricsManager::Get();
+        ImGui::Text("Memory Usage: %s", MemoryUnitLabelHelper(memoryMetricsManager.GetTotalAllocatedBytes() - memoryMetricsManager.GetTotalFreedBytes()).data());
     }
 
     void UnfreedMemoryFromCurrentFrame()
     {
-        Core::FrameAllocationData frameAllocationData = Core::MemoryMetricsManager::Get().GetFrameAllocationData();
+        const Core::FrameAllocationData frameAllocationData = Core::MemoryMetricsManager::Get().GetFrameAllocationData();
         ImGui::Text("Unfreed memory from current frame: %u bytes", (frameAllocationData.AllocatedBytes - frameAllocationData.FreedBytes));
     }
 
     void AllocationsAndFreesForCurrentFrame()
     {
-        Core::FrameAllocationData frameAllocationData = Core::MemoryMetricsManager::Get().GetFrameAllocationData();
+        const Core::FrameAllocationData frameAllocationData = Core::MemoryMetricsManager::Get().GetFrameAllocationData();
         ImGui::Text("Allocations in current frame: %u", frameAllocationData.NumberOfAllocations);
         ImGui::Text("Frees in current frame: %u", frameAllocationData.NumberOfFrees);
     }
