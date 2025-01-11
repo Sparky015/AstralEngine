3b4c8dba6d27d9d793f1c3bfe87bc029907499e6
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-01-11 18:50:57 -0500

    Revised linear allocator and cleaned up main

--- Source/AstralEngine/Core/Memory/Allocators/LinearAllocator.h
+++ Source/AstralEngine/Core/Memory/Allocators/LinearAllocator.h
@@ -7,26 +7,26 @@
 #pragma once
 
 #include <cstddef>
-#include <cstdlib>
 #include <cstring>
 #include <memory>
 #include <new>
-#include <type_traits>
 
 
 namespace Core {
 
+    /**@brief A stack-based linear allocator. Max allocation size is 5.28 KB due to being on the stack.
+     *        Deallocate method does nothing. Reset method deallocates the whole memory block.
+     * @warning You have to use the reset method to deallocate memory. It deallocates all memory being used.
+     *          It's all or nothing. */
     template <typename T, size_t memoryBlockSize>
-    class StackLinearAllocator {
+    class StackLinearAllocator
+    {
     public:
         static constexpr size_t MAX_STACK_ALLOCATION_SIZE = 5280; // 5.28 KB
         static_assert(memoryBlockSize <= MAX_STACK_ALLOCATION_SIZE, "Memory block size for stack is too big!");
 
         using value_type = T;
         using pointer = T*;
-        using const_pointer = const T*;
-        using reference = T&;
-        using const_reference = const T&;
         using size_type = std::size_t;
         using difference_type = std::ptrdiff_t;
         using propagate_on_container_move_assignment = std::true_type;
@@ -34,6 +34,7 @@ namespace Core {
         using is_always_equal = std::false_type; // This NEEDS to be false for stateful allocators!!!
 
 
+        /**@brief Allocates memory for n instances of the type of allocator. Hint is completely ignored. */
         pointer allocate(size_type n, const void* hint = nullptr)
         {
             const size_t allocatedBytes = n * sizeof(T);
@@ -42,7 +43,7 @@ namespace Core {
                 throw std::bad_alloc();
             }
 
-            // Ensure proper alignment
+
             std::size_t space = m_EndBlockAddress - m_CurrentMarker;
             void* alignedAddress = m_CurrentMarker;
             if (std::align(alignof(T), allocatedBytes, alignedAddress, space))
@@ -56,17 +57,20 @@ namespace Core {
         }
 
 
+        /**@brief This does nothing. Use reset method to deallocate memory. */
         void deallocate(pointer ptr, size_type n)
         {
             // Does nothing. Only resets memory on call to reset()
         }
 
+        /**@brief Resets ALL memory that the allocator owns. Everything gets deallocated. */
         void reset()
         {
             memset(m_MemoryBlock, 0, memoryBlockSize);
             m_CurrentMarker = m_StartBlockAddress;
         }
 
+        /**@brief Gets the amount of memory currently allocated out by the allocator. */
         size_t getUsedBlockSize()
         {
             return m_CurrentMarker - m_StartBlockAddress;
@@ -92,6 +96,20 @@ namespace Core {
             m_CurrentMarker = m_StartBlockAddress + (other.m_CurrentMarker - other.m_StartBlockAddress);
         };
 
+
+        StackLinearAllocator& operator=(const StackLinearAllocator& other) noexcept
+        {
+            if (this != &other)
+            {
+                memcpy(m_MemoryBlock, other.m_MemoryBlock, memoryBlockSize);
+                m_StartBlockAddress = m_MemoryBlock;
+                m_EndBlockAddress = m_StartBlockAddress + memoryBlockSize;
+                m_CurrentMarker = m_StartBlockAddress + (other.m_CurrentMarker - other.m_StartBlockAddress);
+            }
+
+            return *this;
+        }
+
         StackLinearAllocator(StackLinearAllocator&& other) noexcept
         {
             std::memcpy(m_MemoryBlock, other.m_MemoryBlock, memoryBlockSize);
@@ -101,8 +119,16 @@ namespace Core {
             other.m_CurrentMarker = other.m_StartBlockAddress;
         };
 
-        StackLinearAllocator& operator=(const StackLinearAllocator& other) = default;
-        StackLinearAllocator& operator=(StackLinearAllocator&& other) = default;
+
+        StackLinearAllocator& operator=(StackLinearAllocator&& other) noexcept
+        {
+            std::memcpy(m_MemoryBlock, other.m_MemoryBlock, memoryBlockSize);
+            m_StartBlockAddress = m_MemoryBlock;
+            m_EndBlockAddress = m_StartBlockAddress + memoryBlockSize;
+            m_CurrentMarker = m_StartBlockAddress + (other.m_CurrentMarker - other.m_StartBlockAddress);
+            other.m_CurrentMarker = other.m_StartBlockAddress;
+            return *this;
+        };
 
 
     private:
@@ -111,19 +137,28 @@ namespace Core {
         unsigned char* m_StartBlockAddress = m_MemoryBlock;
         unsigned char* m_EndBlockAddress = m_StartBlockAddress + memoryBlockSize;
         unsigned char* m_CurrentMarker = m_StartBlockAddress;
+
+        template<typename T1, typename T2, size_t S>
+        friend bool operator==(const StackLinearAllocator<T1, S>& a1, const StackLinearAllocator<T2, S>& a2) noexcept;
+
+        template<typename T1, typename T2, size_t S>
+        friend bool operator!=(const StackLinearAllocator<T1, S>& a1, const StackLinearAllocator<T2, S>& a2) noexcept;
     };
 
 
     template <typename T, typename U, size_t memoryBlockSize>
     bool operator==(const StackLinearAllocator<T, memoryBlockSize>& a1, const StackLinearAllocator<U, memoryBlockSize>& a2) noexcept
     {
-        return false;
+        return (a1.m_CurrentMarker == a2.m_CurrentMarker &&
+            a1.m_MemoryBlock == a2.m_MemoryBlock &&
+            a1.m_EndBlockAddress == a2.m_EndBlockAddress &&
+            a1.m_StartBlockAddress == a2.m_StartBlockAddress);
     }
 
     template <typename T, typename U, size_t memoryBlockSize>
     bool operator!=(const StackLinearAllocator<T, memoryBlockSize>& a1, const StackLinearAllocator<U, memoryBlockSize>& a2) noexcept
     {
-        return false;
+        return !(a1 == a2);
     }
 
 }
--- Source/AstralEngine/main.cpp
+++ Source/AstralEngine/main.cpp
@@ -1,27 +1,12 @@
 #include "Core/Engine.h"
 #include "Debug/EngineDebugInfoLogger.h"
 
-#include "Debug/Macros/Loggers.h" // testing
-#include "Core/Memory/Allocators/LinearAllocator.h"
-
-
-
 
 int main()
 {
     LOG("\n---   Entering main()    ---     (static and global object initialization above)\n");
 
     LOG("Compiled On: " << __DATE__ << " " << __TIME__ << "\n");
-
-    using CustomOStringStream = std::basic_ostringstream<char, std::char_traits<char>, Core::StackLinearAllocator<char, 5024>>;
-
-    Debug::Macros::LinearStringStream os;
-
-    os << "This is a test! jhgkuyfjthdfcgjthfchgfhgfghgknguvgebuyrgvuyergfuvberguygvufyerguyerbguybvuyegruybvrbuyfguyervgbvuygrbyugfveubyvrgbuy";
-
-
-    Debug::Macros::macro_LOG(os);
-
 
     {
         Engine engine = Engine();
