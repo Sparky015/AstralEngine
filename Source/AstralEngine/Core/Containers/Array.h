ec36d260ed45c11d6a041d6845fe6b915524b1bb
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-01-25 02:06:55 -0500

    Implemented array class and unit tests

--- Source/AstralEngine/Core/Containers/Array.h
+++ Source/AstralEngine/Core/Containers/Array.h
@@ -4,17 +4,131 @@
 
 #pragma once
 
+#include "Debug/Macros/Asserts.h"
+#include <cstring>
 #include <memory>
 
 namespace Astral {
 
-    template<typename ElementType, size_t Size>
+    /**@brief Represents a fixed-size c-style array with convenience functions. */
+    template<typename ElementType, size_t ArraySize>
     class Array
     {
     public:
 
+        /**@brief Constructs an array filled with default constructed elements. */
+        constexpr Array() noexcept { Fill(ElementType()); }
+
+        /**@brief Constructs an array filled with the given element. */
+        constexpr explicit Array(const ElementType& elementType) noexcept { Fill(elementType); }
+
+        /**@brief Constructs an array with the given elements in the initializer list */
+        Array(std::initializer_list<ElementType> initializerList)
+        {
+            ASSERT(initializerList.size() == ArraySize, "Size of initializer list and array do not match!")
+            std::copy(initializerList.begin(), initializerList.end(), m_Data);
+        };
+
+        /**@brief Assigns the array instance the given elements in the initializer list */
+        Array& operator=(std::initializer_list<ElementType> initializerList)
+        {
+            ASSERT(initializerList.size() == ArraySize, "Size of initializer list and array do not match!")
+            std::copy(initializerList.begin(), initializerList.end(), m_Data);
+            return *this;
+        }
+
+        /**@brief Fills the array with the given element.
+         * @param fillElement The element to set the array to. */
+        constexpr void Fill(const ElementType& fillElement) noexcept
+        {
+            std::fill_n(Begin(), ArraySize, fillElement);
+        }
+
+        /**@brief Checks if the given element is in the array.
+         * @param element The element that will be checked for.
+         * @return True if the element is in the array, false otherwise. */
+        constexpr bool Contains(const ElementType& element) const noexcept
+        {
+            for (size_t i = 0; i < ArraySize; i++)
+            {
+                if (m_Data[i] == element) { return true; }
+            }
+            return false;
+        }
+
+        /**@brief Gets the size of the array. */
+        [[nodiscard]] constexpr size_t Size() const noexcept { return ArraySize; }
+
+        /**@brief Gets the last index of the array. */
+        [[nodiscard]] constexpr size_t EndIndex() const noexcept { return ArraySize - 1; }
+
+        /**@brief Gets the raw pointer of the internal c-style array. */
+        [[nodiscard]] constexpr ElementType* RawData() noexcept { return m_Data; }
+
+        /**@brief Gets the raw pointer of the internal c-style array. */
+        [[nodiscard]] constexpr const ElementType* RawData() const noexcept { return m_Data; }
+
+        /**@brief Reads an element at the given index. */
+        [[nodiscard]] ElementType& Index(size_t index)
+        {
+            ASSERT(index < ArraySize, "Index is out of bounds!");
+            return m_Data[index];
+        }
+
+        /**@brief Reads an element at the given index. */
+        [[nodiscard]] ElementType& operator[](size_t index)
+        {
+            ASSERT(index < ArraySize, "Index is out of bounds!");
+            return m_Data[index];
+        }
+
+        /**@brief Reads an element at the given index. */
+        [[nodiscard]] const ElementType& operator[](size_t index) const
+        {
+            ASSERT(index < ArraySize, "Index is out of bounds!");
+            return m_Data[index];
+        }
+
+        // Iterators (lowercase too for STL compatability)
+        constexpr ElementType* Begin() noexcept { return &m_Data[0]; }
+        constexpr ElementType* End() noexcept { return &m_Data[ArraySize]; }
+        constexpr const ElementType* Begin() const noexcept{ return &m_Data[0]; }
+        constexpr const ElementType* End() const noexcept { return &m_Data[ArraySize]; }
+        constexpr ElementType* begin() noexcept{ return Begin(); }
+        constexpr ElementType* end() noexcept { return End(); }
+        constexpr const ElementType* begin() const noexcept { return Begin(); }
+        constexpr const ElementType* end() const noexcept { return End(); }
+
+
+        /**@brief Compares one array with another array
+         * @warning The memory layout is compared and custom equality operators will be ignored for trivially copyable types. */
+        bool operator==(const Array& other) const
+        {
+            if constexpr (std::is_trivially_copyable_v<ElementType>)
+            {
+                return std::memcmp(m_Data, other.m_Data, ArraySize * sizeof(ElementType)) == 0;
+            }
+            else
+            {
+                for (size_t i = 0; i < ArraySize; i++)
+                {
+                    if (m_Data[i] != other.m_Data[i]) { return false; }
+                }
+            }
+            return true;
+        }
+
+        using value_type = ElementType;
+        using size_type = std::size_t;
+        using difference_type = std::ptrdiff_t;
+        using reference = ElementType&;
+        using const_reference = const ElementType&;
+        using pointer = ElementType*;
+        using const_pointer = const ElementType*;
+
     private:
-        ElementType m_Buffer[Size];
+
+        ElementType m_Data[ArraySize];
     };
 
 }
--- Tests/AstralEngine/Core/Containers/ArrayTest.cpp
+++ Tests/AstralEngine/Core/Containers/ArrayTest.cpp
@@ -8,6 +8,93 @@
 class ArrayTest : public ::testing::Test
 {
 public:
-
+    Astral::Array<int, 5> m_DefaultArray;
 };
 
+TEST_F(ArrayTest, DefaultConstructor_FillsDefaultInstances)
+{
+    EXPECT_EQ(m_DefaultArray.Index(0), 0);
+    EXPECT_EQ(m_DefaultArray.Index(2), 0);
+    EXPECT_EQ(m_DefaultArray.Index(4), 0);
+
+    Astral::Array<int, 5> expected{0, 0, 0, 0, 0};
+    EXPECT_EQ(m_DefaultArray, expected);
+}
+
+TEST_F(ArrayTest, SingleElementConstructor_FillsArrayWithSingleGivenInstance)
+{
+    Astral::Array<int, 5> array = Astral::Array<int, 5>(20);
+    EXPECT_EQ(array.Index(0), 20);
+    EXPECT_EQ(array.Index(2), 20);
+    EXPECT_EQ(array.Index(4), 20);
+
+    Astral::Array<int, 5> expected{20, 20, 20, 20, 20};
+    EXPECT_EQ(array, expected);
+}
+
+TEST_F(ArrayTest, InitializerListConstructor_FillsArrayWithGivenValues)
+{
+    Astral::Array<int, 5> array = Astral::Array<int, 5>(20);
+    EXPECT_EQ(array.Index(0), 20);
+    EXPECT_EQ(array.Index(2), 20);
+    EXPECT_EQ(array.Index(4), 20);
+
+    Astral::Array<int, 5> expected{20, 20, 20, 20, 20};
+    EXPECT_EQ(array, expected);
+}
+
+TEST_F(ArrayTest, InitializerListOperator_FillsArrayWithGivenValues)
+{
+    Astral::Array<int, 5> array = Astral::Array<int, 5>(20);
+    EXPECT_EQ(array.Index(0), 20);
+    EXPECT_EQ(array.Index(2), 20);
+    EXPECT_EQ(array.Index(4), 20);
+
+    array = {30, 50, 501, 10, 70};
+    EXPECT_EQ(array.Index(0), 30);
+    EXPECT_EQ(array.Index(2), 501);
+    EXPECT_EQ(array.Index(4), 70);
+}
+
+TEST_F(ArrayTest, Fill_PopulatesArrayWithSingleInstance)
+{
+    Astral::Array<int, 5> array = Astral::Array<int, 5>({23, 1, 5, 6, 8});
+    array.Fill(2);
+    EXPECT_EQ(array.Index(0), 2);
+    EXPECT_EQ(array.Index(2), 2);
+    EXPECT_EQ(array.Index(4), 2);
+
+    Astral::Array<int, 5> expected{23, 1, 5, 6, 8};
+    EXPECT_NE(array, expected);
+    expected.Fill(2);
+    EXPECT_EQ(array, expected);
+}
+
+TEST_F(ArrayTest, Contains_CorrectlyIdentifiesContainedElements)
+{
+    Astral::Array<int, 5> array = Astral::Array<int, 5>({23, 1, 5, 6, 8});
+
+    EXPECT_EQ(array.Contains(23), true);
+    EXPECT_EQ(array.Contains(1), true);
+    EXPECT_EQ(array.Contains(5), true);
+    EXPECT_EQ(array.Contains(6), true);
+    EXPECT_EQ(array.Contains(8), true);
+
+    EXPECT_EQ(array.Contains(0), false);
+    EXPECT_EQ(array.Contains(-1), false);
+    EXPECT_EQ(array.Contains(7), false);
+    EXPECT_EQ(array.Contains(9), false);
+    EXPECT_EQ(array.Contains(-5), false);
+}
+
+TEST_F(ArrayTest, Iterators_CorrectlyIteratesThroughContainer)
+{
+    Astral::Array<int, 8> array = {2, 3, 4, 5, 6, 78, 8, 0};
+
+    size_t forloopIndex = 0;
+    for (int i : array)
+    {
+        EXPECT_EQ(array[forloopIndex], i);
+        forloopIndex++;
+    }
+}
