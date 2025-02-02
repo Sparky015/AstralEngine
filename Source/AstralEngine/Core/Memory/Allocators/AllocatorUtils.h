8c0e0c0ad1d4ec3ff5bd7bad9f9d105ca107894f
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-02-02 00:46:20 -0500

    Implemented AlignedAlloc method and AlignedFree method for a portable aligned allocation method.
    Added unit tests for allocator utils

--- Source/AstralEngine/Core/Memory/Allocators/AllocatorUtils.h
+++ Source/AstralEngine/Core/Memory/Allocators/AllocatorUtils.h
@@ -6,24 +6,23 @@
 
 #pragma once
 
+//#include "Debug/Macros/Loggers.h"
 #include <cstdlib>
-#include "Debug/Macros/Loggers.h"
 
-#ifdef ASTRAL_PLATFORM_WINDOWS
-#define ALLOCATOR_UTILS_ALIGNED_ALLOC(alignment, size) _aligned_malloc(size, alignment)
-#define ALLOCATOR_UTILS_ALIGNED_FREE(pointer) _aligned_free(pointer)
-#else
-#define ALLOCATOR_UTILS_ALIGNED_ALLOC(alignment, size) std::aligned_alloc(alignment, size)
-#define ALLOCATOR_UTILS_ALIGNED_FREE(pointer) std::free(pointer)
-#endif
+#include "Core/CoreMacroDefinitions.h"
+#include "Debug/Macros/Asserts.h"
 
 namespace Core::AllocatorUtils {
 
     /**@brief Checks if a given alignment is a power of two.
      * @param alignment The alignment being checked
-     * @return True if the alignment is a power of two, and false otherwise. */
+     * @return True if the alignment is a power of two, and false otherwise.
+     * @warning Alignments should not be zero. Passing in zero as an argument
+     *          with return the wrong answer. Do not pass in zero.
+     *  @thread_safety This function is not thread safe. */
     [[nodiscard]] constexpr bool IsAlignmentPowerOfTwo(const size_t alignment) noexcept
     {
+        [[unlikely]] if (alignment == 0) { return false; }
         return (alignment & (alignment - 1)) == 0;
     }
 
@@ -31,7 +30,8 @@ namespace Core::AllocatorUtils {
      * @param currentAddress The current address marker of an allocator
      * @param numberOfBytes The number of bytes that will be allocated
      * @param endAddress The end address of the allocator memory block
-     * @return True if allocating the given number of bytes will overflow the allocator memory block, and false otherwise. */
+     * @return True if allocating the given number of bytes will overflow the allocator memory block, and false otherwise.
+     *  @thread_safety This function is not thread safe. */
     [[nodiscard]] constexpr bool DoesCauseOverflow(void* currentAddress, const size_t numberOfBytes, void* endAddress) noexcept
     {
         return (static_cast<unsigned char*>(currentAddress) + numberOfBytes) > static_cast<unsigned char*>(endAddress);
@@ -42,10 +42,76 @@ namespace Core::AllocatorUtils {
      * @param originalBlockSize The original block size of the allocator.
      *                          Expected to be not zero. Can't be negative because of size_t.
      * @param alignment The alignment that the memory block will be aligned to
-     * @return A block size that is the next multiple of the alignment. */
+     * @return A block size that is the next multiple of the alignment. Returns 0 if given alignment is 0.
+     *  @thread_safety This function is not thread safe. */
     constexpr size_t RoundToNextAlignmentMultiple(size_t originalBlockSize, size_t alignment)
     {
+        [[unlikely]] if (alignment == 0) { return 0; }
         return ((originalBlockSize + alignment - 1) / alignment) * alignment;
+    }
+
+    /** @brief Allocates a memory block which has the given alignment
+     *  @param size The size of the memory block to allocate
+     *  @param alignment The alignment of the allocated memory block. Must be a power of two.
+     *  @return A pointer to the allocated memory block. Returns nullptr if size is zero.
+     *  @warning The max alignment support through this function is 255.
+     *  @thread_safety This function is not thread safe. */
+    inline void* AlignedAlloc(size_t size, uint8 alignment)
+    {
+        [[unlikely]] if (size == 0) { return nullptr; }
+
+        ASSERT(alignment < 256, "Alignment is too big. This function supports up to 255 for the alignment.");
+        ASSERT(IsAlignmentPowerOfTwo(alignment), "Alignment must be a power of two.")
+
+        // Giving ourselves enough space to perform an alignment and for an allocator header to track the offset
+        size_t RoundedBlockSize = size + alignment;
+        size_t space = RoundedBlockSize;
+
+        void* allocatedAddress = std::malloc(RoundedBlockSize);
+        [[unlikely]] if (allocatedAddress == nullptr) { return nullptr; }
+
+        void* alignedAddress = allocatedAddress;
+
+        // Aligns the address and will return nullptr if there is not enough space
+        alignedAddress = std::align(alignment, size, alignedAddress, space);
+
+        [[unlikely]] if (alignedAddress == nullptr)
+        {
+            std::free(allocatedAddress);
+            return nullptr;
+        }
+
+        ASSERT(space >= size, "The aligning of the address caused the block to be smaller than the user asked for.")
+
+        if ((unsigned char*)alignedAddress == allocatedAddress)
+        {
+            // Address is already aligned. Push the address by the given alignment to make room for allocation header.
+            alignedAddress = (unsigned char*)alignedAddress + alignment;
+        }
+
+        // Add allocation header for alignment amount
+        unsigned char* m_HeaderMarker = (unsigned char*)alignedAddress - 1;
+        const uint8 alignmentOffset = (unsigned char*)alignedAddress - (unsigned char*)allocatedAddress;
+        *(m_HeaderMarker) = alignmentOffset;
+
+        return alignedAddress;
+    }
+
+
+    /** @brief Frees a memory block allocated by AllocatorUtils::AlignedAlloc.
+     *  @param pointer The pointer to the memory block being freed
+     *  @warning This free method can only be used to free pointers allocated by
+     *           AllocatorUtils::AlignedAlloc and ONLY that method.
+     *  @thread_safety This function is not thread safe. */
+    inline void FreeAlignedAlloc(void* pointer)
+    {
+        [[unlikely]] if (pointer == nullptr) { return; }
+
+        // Get the natural alignment offset size from the allocation header
+        unsigned char* headerMarker = static_cast<unsigned char*>(pointer) - 1;
+        uint8 alignmentOffset = *headerMarker;
+
+        std::free((unsigned char*)pointer - alignmentOffset);
     }
 
     /** @brief Allocates a max aligned memory block that is at least the size of originalBlockSize.
@@ -53,11 +119,11 @@ namespace Core::AllocatorUtils {
      *  @return A pointer to a max aligned memory block
      *  @note - The actual block size of the max aligned memory block might be slightly bigger due to adjusting the size
      *        of the block to make it a multiple of the alignment.
-     *        - Use AllocatorUtils::FreeMaxAlignedBlock to free memory allocated by this function. */
+     *        - Use AllocatorUtils::FreeMaxAlignedBlock to free memory allocated by this function.
+     *  @thread_safety This function is not thread safe. */
     inline void* AllocMaxAlignedBlock(size_t originalBlockSize)
     {
-        const size_t alignedBlockSize = RoundToNextAlignmentMultiple(originalBlockSize, alignof(max_align_t));
-        return ALLOCATOR_UTILS_ALIGNED_ALLOC(alignof(std::max_align_t), alignedBlockSize);
+        return AlignedAlloc(originalBlockSize, alignof(max_align_t));
     }
 
     /** @brief Frees a max aligned memory block that was allocated by AllocatorUtils::AllocMaxAlignedBlock.
@@ -65,26 +131,26 @@ namespace Core::AllocatorUtils {
      *  @note This function ensures the corresponding free function for std::aligned_alloc is called. */
     inline void FreeMaxAlignedBlock(void* blockPtr)
     {
-        ALLOCATOR_UTILS_ALIGNED_FREE(blockPtr);
+        FreeAlignedAlloc(blockPtr);
     }
 
     /** @brief Resizes an allocator's memory block to a bigger size.
      *  @param memoryBlockPtr Pointer to the allocator's memory block.
      *  @param memoryBlockSize The current size of the allocator's memory block
      *  @param resizeMultiplier The amount the original memory block size is multiplied by to get the new memory block's size.
-     *  @return A pointer to the new allocated block that is resized by the given. */
-    inline void* ResizeMemoryBlock(void* memoryBlockPtr, size_t memoryBlockSize, float resizeMultiplier = 2.0f)
+     *  @return A pointer to the new allocated block that is resized by the given. Returns nullptr if resize allocation failed.
+     *  @warning This does not copy any data over. It is intended to be used when the allocator is empty.
+     *  @thread_safety This function is not thread safe. */
+    inline void ResizeMemoryBlock(void* memoryBlockPtr, size_t memoryBlockSize, void*& outNewMemoryBufferPointer, size_t& newMemoryBufferSize)
     {
-        const size_t newBlockSize = memoryBlockSize * resizeMultiplier;
-        void* newBlockPtr = AllocMaxAlignedBlock(newBlockSize);
-        std::memcpy(newBlockPtr, memoryBlockPtr, memoryBlockSize);
+        static constexpr float resizeMultiplier = 2.0f;
+        newMemoryBufferSize = memoryBlockSize * resizeMultiplier;
+        outNewMemoryBufferPointer = AllocMaxAlignedBlock(newMemoryBufferSize);
+        if (!outNewMemoryBufferPointer) { return; }
         FreeMaxAlignedBlock(memoryBlockPtr);
 
-        WARN("Resizing Allocator Memory Block! (" << memoryBlockSize << " bytes -> " << newBlockSize << " bytes)");
-        return newBlockPtr;
+//        WARN("Resizing Allocator Memory Block! (" << memoryBlockSize << " bytes -> " << newBlockSize << " bytes)");
     }
 
 }
 
-#undef ALLOCATOR_UTILS_ALIGNED_ALLOC
-#undef ALLOCATOR_UTILS_ALIGNED_FREE
