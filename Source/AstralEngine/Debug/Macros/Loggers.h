760f6f3ee8e70acebc39d8984f7f310219332245
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-02-02 17:32:59 -0500

    Changed WARN macro to display file name instead of file path

--- Source/AstralEngine/Debug/Macros/Loggers.h
+++ Source/AstralEngine/Debug/Macros/Loggers.h
@@ -6,8 +6,6 @@
 
 #include <sstream>
 
-#include "Core/Memory/Allocators/STLAllocators/STLLinearAllocator.h"
-
 #ifndef TURN_OFF_DEBUG_MACROS
 /** Outputs the message to the console with a time stamp. */
 #define TRACE(message) { std::ostringstream ss; ss << message; Debug::Macros::macro_TRACE(ss); }
@@ -16,7 +14,7 @@
 #define LOG(message) { std::ostringstream ss; ss << message; Debug::Macros::macro_LOG(ss); }
 
 /** Logs warnings (that can be a stream) to the console with a yellow color. */
-#define WARN(message) { std::ostringstream ss;  ss << message; Debug::Macros::macro_WARN(ss, __FILE__, __LINE__); }
+#define WARN(message) { std::ostringstream ss;  ss << message; Debug::Macros::macro_WARN(ss, __FILE_NAME__, __LINE__); }
 
 
 #else
@@ -28,47 +26,11 @@
 
 namespace Debug::Macros {
 
-class LinearStringStream
-{
-public:
-    LinearStringStream() = default;
-    ~LinearStringStream() = default;
-
-
-    void reset()
-    {
-//        m_Stream.str("");
-        m_Stream.clear();
-        m_Stream.rdbuf()->get_allocator().reset();
-    }
-
-
-    std::string_view view() const
-    {
-        return m_Stream.view();
-    }
-
-
-    // Convenience operator for streaming
-    template<typename T>
-    LinearStringStream& operator<<(const T& value) {
-        m_Stream << value;
-        return *this;
-    }
-
-private:
-    using StreamType = std::basic_ostringstream<char, std::char_traits<char>, Core::STLLinearAllocator<char>>;
-    StreamType m_Stream;
-
-};
     /** Outputs the message to the console with a time stamp [hr:min:sec:ms]. */
     void macro_TRACE(const std::ostringstream& message);
 
     /** A simple log to console that can take a stream while also standardizing which side of the string the new line character is on. */
     void macro_LOG(const std::ostringstream& message);
-    void macro_LOG(const LinearStringStream& message);
-
-
 
     /** Logs warnings (that can be a stream) to the console with a yellow color. */
     void macro_WARN(const std::ostringstream& message, const char* file, int line);
