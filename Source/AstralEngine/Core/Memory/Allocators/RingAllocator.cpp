1f67b99842d2ce312d10ab413ff368188834ccb3
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-02-04 22:54:45 -0500

    - Added README for allocator directory - Made files for Ring Allocator

---
+++ Source/AstralEngine/Core/Memory/Allocators/RingAllocator.cpp
@@ -0,0 +1,6 @@
+/**
+* @file RingAllocator.cpp
+* @author Andrew Fagan
+* @date 2/4/2025
+*/
+#include "RingAllocator.h"
---
+++ Source/AstralEngine/Core/Memory/Allocators/RingAllocator.h
@@ -0,0 +1,8 @@
+/**
+* @file RingAllocator.h
+* @author Andrew Fagan
+* @date 2/4/2025
+*/
+
+#pragma once
+
