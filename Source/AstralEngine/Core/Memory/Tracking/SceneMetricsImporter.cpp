3ef8a9ab3e52ba2345ad21979089daaed03f7005
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-02-09 00:34:58 -0500

    Created some of the files for the start of the specification

---
+++ Source/AstralEngine/Core/Memory/Tracking/SceneMetricsImporter.cpp
@@ -0,0 +1,6 @@
+/**
+* @file SceneMetricsImporter.cpp
+* @author Andrew Fagan
+* @date 2/8/2025
+*/
+#include "SceneMetricsImporter.h"
---
+++ Source/AstralEngine/Core/Memory/Tracking/SceneMetricsImporter.h
@@ -0,0 +1,19 @@
+/**
+* @file SceneMetricsImporter.h
+* @author Andrew Fagan
+* @date 2/8/2025
+*/
+
+#pragma once
+
+namespace Core {
+
+    class SceneMetricsImporter
+    {
+    public:
+
+    private:
+
+    };
+
+}
