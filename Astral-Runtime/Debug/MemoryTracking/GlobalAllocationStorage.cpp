22e6c10e26be83e8d429dcb5785b7e3a1d22f85f
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-04-15 11:56:16 -0400

    Added ASAN back and added new lines characters to the leaked pointers output

--- Astral-Runtime/Debug/MemoryTracking/GlobalAllocationStorage.cpp
+++ Astral-Runtime/Debug/MemoryTracking/GlobalAllocationStorage.cpp
@@ -5,26 +5,24 @@
 */
 
 #include "Debug/Utilities/Asserts.h"
-#include "Debug/Utilities/Loggers.h"
 #include "GlobalAllocationStorage.h"
 
-
 #include <iostream>
-
 
 namespace Core {
 
     GlobalAllocationStorage::~GlobalAllocationStorage()
     {
         // Output all the pointers still being stored as leaked.
-        std::cout << "Number of leaked pointers: " << m_NumberOfEntries;
+        std::cout << "Number of leaked pointers: " << m_NumberOfEntries << "\n";
         for (auto [pointer, allocationData] : m_Storage)
         {
             if (allocationData.threadID == std::this_thread::get_id())
             {
-                std::cout << "Leaked pointer " << pointer << " of size " << allocationData.size;
+                std::cout << "Leaked pointer " << pointer << " of size " << allocationData.size << "\n";
             }
         }
+        // std::cout << "Number of leaked pointers: " << m_NumberOfEntries << "\n";
 
     }
 
