30b75f1c319cfe8d927b88812489b4f032d31340
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-07-14 12:02:21 -0400

    Added new abstractions for dependency flags, image subresource range and pipeline barriers

---
+++ Astral-Runtime/Renderer/RHI/Common/ImageSubresourceRange.h
@@ -0,0 +1,35 @@
+/**
+* @file ImageSubResourceRange.h
+* @author Andrew Fagan
+* @date 7/13/2025
+*/
+
+#pragma once
+
+#include "Core/FixedIntegerTypes.h"
+
+namespace Astral {
+
+    enum ImageAspectFlagBits : uint32
+    {
+        IMAGE_ASPECT_COLOR_BIT      =   1u,
+        IMAGE_ASPECT_DEPTH_BIT      =   1u << 1,
+        IMAGE_ASPECT_STENCIL_BIT    =   1u << 2,
+        IMAGE_ASPECT_METADATA_BIT   =   1u << 3,
+        IMAGE_ASPECT_PLANE_0_BIT    =   1u << 4,
+        IMAGE_ASPECT_PLANE_1_BIT    =   1u << 5,
+        IMAGE_ASPECT_PLANE_2_BIT    =   1u << 6,
+    };
+
+    using ImageAspectFlags = uint32;
+
+    struct ImageSubresourceRange
+    {
+        ImageAspectFlags AspectMask;
+        uint32 BaseMipLevel;
+        uint32 LevelCount;
+        uint32 BaseArrayLayer;
+        uint32 LayerCount;
+    };
+
+}
---
+++ Astral-Runtime/Renderer/RHI/Common/PipelineBarriers.h
@@ -0,0 +1,59 @@
+/**
+* @file PipelineBarriers.h
+* @author Andrew Fagan
+* @date 7/13/2025
+*/
+
+
+#pragma once
+
+#include "AccessFlags.h"
+#include "DependencyFlags.h"
+#include "ImageLayouts.h"
+#include "ImageSubResourceRange.h"
+#include "PipelineStageFlags.h"
+#include "Renderer/RHI/Resources/Buffer.h"
+#include "Renderer/RHI/Resources/Texture.h"
+
+namespace Astral {
+
+    struct MemoryBarrier
+    {
+        AccessFlags SourceAccessMask;
+        AccessFlags DestinationAccessMask;
+    };
+
+    struct ImageMemoryBarrier
+    {
+        AccessFlags SourceAccessMask;
+        AccessFlags DestinationAccessMask;
+        ImageLayout OldLayout;
+        ImageLayout NewLayout;
+        uint32 SourceQueueFamilyIndex;
+        uint32 DestinationQueueFamilyIndex;
+        TextureHandle Image;
+        ImageSubresourceRange ImageSubresourceRange;
+    };
+
+    struct BufferMemoryBarrier
+    {
+        AccessFlags SourceAccessMask;
+        AccessFlags DestinationAccessMask;
+        uint32 SourceQueueFamilyIndex;
+        uint32 DestinationQueueFamilyIndex;
+        BufferHandle Buffer;
+        uint32 Offset;
+        uint32 Size;
+    };
+
+    struct PipelineBarrier
+    {
+        PipelineStageFlags SourceStageMask;
+        PipelineStageFlags DestinationStageMask;
+        DependencyFlags DependencyFlags;
+        std::vector<MemoryBarrier> MemoryBarriers;
+        std::vector<BufferMemoryBarrier> BufferMemoryBarriers;
+        std::vector<ImageMemoryBarrier> ImageMemoryBarriers;
+    };
+
+}
