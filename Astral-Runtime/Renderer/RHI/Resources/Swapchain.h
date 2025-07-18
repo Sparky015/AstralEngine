656200072e0a50e0f7334be26aa9e99631660db3
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2025-07-18 16:48:12 -0400

    Added documentation for Swapchain and VulkanSwapchain

--- Astral-Runtime/Renderer/RHI/Resources/Swapchain.h
+++ Astral-Runtime/Renderer/RHI/Resources/Swapchain.h
@@ -19,13 +19,36 @@ namespace Astral {
     public:
         virtual ~Swapchain() = default;
 
+        /**
+         * @brief  Acquires the next render target from the swapchain and returns it
+         * @return The acquired render target
+         */
         virtual GraphicsRef<RenderTarget> AcquireNextImage() = 0;
 
+        /**
+         * @brief  Gets the number of render targets in the swapchain
+         * @return The number of render targets in the swapchain
+         */
         virtual uint32 GetNumberOfImages() = 0;
+
+        /**
+         * @brief  Gets the render targets in the swapchain
+         * @return The render targets in the swapchain
+         */
         virtual std::vector<RenderTargetHandle>& GetRenderTargets() = 0;
 
+        /**
+         * @brief Recreates the swapchain with the specified dimensions
+         * @param width  The width of the new swapchain images
+         * @param height The height of the new swapchain images
+         */
         virtual void RecreateSwapchain(uint32 width, uint32 height) = 0;
 
+        /**
+         * @brief  Gets the native swapchain handle
+         * @return The native swapchain handle
+         * @note   The void pointer maps to the native swapchain handle of the selected renderer api backend
+         */
         virtual void* GetNativeHandle() = 0;
     };
 
