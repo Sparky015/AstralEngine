f8ec12569e10e1d1120148749b204410d2014863
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-04-06 10:37:35 -0400

    [AE-61] Implemented the Metal Buffer

--- Astral-Runtime/Renderer/RHI/Platform/Metal/Resources/MetalBuffer.cpp
+++ Astral-Runtime/Renderer/RHI/Platform/Metal/Resources/MetalBuffer.cpp
@@ -6,57 +6,220 @@
 
 #include "MetalBuffer.h"
 
+#include "Debug/Utilities/Asserts.h"
+#include "Debug/Utilities/Loggers.h"
+#include "Metal/MTLBlitCommandEncoder.hpp"
+#include "Metal/MTLCommandBuffer.hpp"
+
+#include "Metal/MTLDevice.hpp"
+
 namespace Astral {
 
-    MetalBuffer::MetalBuffer(const MetalBufferDesc& metalBufferDesc)
+    MetalBuffer::MetalBuffer(const MetalBufferDesc& metalBufferDesc) :
+        m_Device(metalBufferDesc.Device),
+        m_Buffer(nullptr),
+        m_BufferLength(0),
+        m_UsedMemorySize(0),
+        m_MemoryType(metalBufferDesc.MemoryType)
     {
+        ASSERT(metalBufferDesc.Device, "[MetalBuffer::MetalBuffer] Device argument was null!")
 
+        CreateBuffer(&m_Buffer, metalBufferDesc.Size);
+        m_BufferLength = m_Buffer->allocatedSize();
+    }
+
+
+    MetalBuffer::~MetalBuffer()
+    {
+        DestroyBuffer(m_Buffer);
     }
 
 
     uint32 MetalBuffer::GetAllocatedSize()
     {
+        if (!m_Buffer) { AE_WARN("Buffer has not been created! Can't get allocated size!"); return 0; }
+        return m_Buffer->allocatedSize();
     }
 
 
     uint32 MetalBuffer::GetUsedSize()
     {
+        return m_UsedMemorySize;
     }
 
 
     void MetalBuffer::ReallocateMemory(uint32 newSize)
     {
+        if (newSize <= m_BufferLength)
+        {
+            AE_WARN("New size is less than or equal to current buffer size. Skipping reallocation!")
+            return;
+        }
+
+        if (m_Buffer->storageMode() == MTL::StorageModePrivate)
+        {
+            AE_WARN("[MetalBuffer::ReallocateMemory] Buffer does not support read/write from CPU!")
+            return;
+        }
+
+        // Transfer data to a newly created buffer of the new size
+        MTL::Buffer* newBuffer;
+        CreateBuffer(&newBuffer, newSize);
+
+        void* currentBufferMemory;
+        MapPointer(&currentBufferMemory);
+
+        memcpy(newBuffer->contents(), currentBufferMemory, m_UsedMemorySize);
+
+        UnmapPointer();
+
+        // Clean up old buffer and update variables to new buffer
+
+        DestroyBuffer(m_Buffer);
+        m_BufferLength = newBuffer->allocatedSize();
+        m_Buffer = newBuffer;
     }
 
 
     void MetalBuffer::MapPointer(void** cpuPtr)
     {
+        if (!cpuPtr)
+        {
+            AE_WARN("[MetalBuffer::MapPointer] Can't map to nullptr")
+            return;
+        }
+
+        if (!m_Buffer)
+        {
+            AE_WARN("[MetalBuffer::MapPointer] Buffer has not been created! Can't map pointer to buffer!")
+            return;
+        }
+
+        if (m_Buffer->storageMode() == MTL::StorageModePrivate)
+        {
+            AE_WARN("[MetalBuffer::MapPointer] Buffer does not support read/write from CPU on private memory!")
+            return;
+        }
+
+        *cpuPtr = m_Buffer->contents();
     }
 
 
     void MetalBuffer::UnmapPointer()
     {
+        // Not supported by Metal
     }
 
 
     void MetalBuffer::CopyDataToBuffer(void* data, uint32 size)
     {
+        if (!data || size == 0) { return; }
+
+        ASSERT(size <= m_BufferLength, "Data does not fit in buffer!")
+
+        if (m_Buffer->storageMode() == MTL::StorageModePrivate)
+        {
+            AE_WARN("[MetalBuffer::CopyDataToBuffer] Buffer does not support read/write from CPU!")
+            return;
+        }
+
+        void* memory = nullptr;
+        MapPointer(&memory);
+        memcpy(memory, data, size);
+        UnmapPointer();
+
+        m_UsedMemorySize = size;
     }
 
 
     void* MetalBuffer::GetNativeHandle()
     {
+        return m_Buffer;
     }
 
 
-    void MetalBuffer::CreateBuffer()
+    void MetalBuffer::CopyFromStagingBuffer(Device& device, Buffer& stagingBuffer, uint32 size)
     {
-        m_Buffer
+        ASSERT(size <= m_BufferLength, "Data does not fit in buffer!")
+        MTL::Buffer* metalStagingBuffer = (MTL::Buffer*)stagingBuffer.GetNativeHandle();
+
+        CommandBufferHandle commandBufferHandle = device.AllocateCommandBuffer();
+        MTL::CommandBuffer* commandBuffer = (MTL::CommandBuffer*)commandBufferHandle->GetNativeHandle();
+
+
+        commandBufferHandle->BeginRecording();
+        MTL::BlitCommandEncoder* blitEncoder = commandBuffer->blitCommandEncoder();
+
+        blitEncoder->copyFromBuffer(metalStagingBuffer, 0, m_Buffer, 0, size);
+
+        blitEncoder->endEncoding();
+        commandBufferHandle->EndRecording();
+
+        CommandQueueHandle commandQueueHandle = device.GetPrimaryCommandQueue();
+        commandQueueHandle->SubmitSync(commandBufferHandle);
+        commandQueueHandle->WaitIdle();
     }
 
 
-    void MetalBuffer::DestroyBuffer()
+    MetalBuffer::MetalBuffer(MetalBuffer&& other) noexcept :
+        m_Device(other.m_Device),
+        m_Buffer(other.m_Buffer),
+        m_BufferLength(other.m_BufferLength),
+        m_UsedMemorySize(other.m_UsedMemorySize),
+        m_MemoryType(other.m_MemoryType)
     {
+        other.m_Device = nullptr;
+        other.m_Buffer = nullptr;
+        other.m_BufferLength = 0;
+        other.m_UsedMemorySize = 0;
+    }
+
+
+    MetalBuffer& MetalBuffer::operator=(MetalBuffer&& other) noexcept
+    {
+        m_Device = other.m_Device;
+        m_Buffer = other.m_Buffer;
+        m_BufferLength = other.m_BufferLength;
+        m_UsedMemorySize = other.m_UsedMemorySize;
+        m_MemoryType = other.m_MemoryType;
+
+        other.m_Device = nullptr;
+        other.m_Buffer = nullptr;
+        other.m_BufferLength = 0;
+        other.m_UsedMemorySize = 0;
+
+        return *this;
+    }
+
+
+    void MetalBuffer::CreateBuffer(MTL::Buffer** outBuffer, uint32 bufferLength)
+    {
+        if (!outBuffer)
+        {
+            AE_WARN("[MetalBuffer::CreateBuffer] Can't create new buffer to null address")
+            return;
+        }
+
+        MTL::ResourceOptions resourceOptions = MTL::ResourceOptions();
+
+        switch (m_MemoryType)
+        {
+            case GPUMemoryType::DEVICE_LOCAL: resourceOptions |= MTL::ResourceStorageModePrivate; break;
+            case GPUMemoryType::HOST_VISIBLE: resourceOptions |= MTL::ResourceStorageModeShared; break;
+            default: AE_ERROR("[MetalBuffer::CreateBuffer] Given memory type is not implemented!")
+        }
+
+        *outBuffer = m_Device->newBuffer(bufferLength, resourceOptions);
+        ASSERT(*outBuffer, "MetalBuffer failed to be created!")
+    }
+
+
+    void MetalBuffer::DestroyBuffer(MTL::Buffer* buffer)
+    {
+        if (buffer)
+        {
+            buffer->release();
+        }
     }
 
 }
--- Astral-Runtime/Renderer/RHI/Platform/Metal/Resources/MetalBuffer.h
+++ Astral-Runtime/Renderer/RHI/Platform/Metal/Resources/MetalBuffer.h
@@ -7,37 +7,114 @@
 #pragma once
 
 #include "Metal/MTLBuffer.hpp"
+#include "Renderer/RHI/Common/MemoryTypes.h"
 #include "Renderer/RHI/Resources/Buffer.h"
+#include "Renderer/RHI/Resources/Device.h"
 
 namespace Astral {
 
+    /**
+     * @brief A description of how to build a Metal buffer
+     */
     struct MetalBufferDesc
     {
         MTL::Device* Device;
+        uint32 Size;
+        GPUMemoryType MemoryType;
     };
 
+    /**
+     * @brief A wrapper around a Metal buffer supporting extra convenience functions
+     */
     class MetalBuffer : public Buffer
     {
     public:
         MetalBuffer(const MetalBufferDesc& metalBufferDesc);
-        virtual ~MetalBuffer() = default;
+        ~MetalBuffer() override;
 
+        /**
+         * @brief Gets the amount of memory allocated to the buffer
+         * @return The amount of memory used in the buffer
+         */
         uint32 GetAllocatedSize() override;
+
+        /**
+         * @brief Gets the amount of memory written to in the buffer
+         * @return The amount of memory used in the buffer
+         */
         uint32 GetUsedSize() override;
+
+        /**
+         * @brief Reallocates the length of a buffer by creating a new buffer and copying data over
+         * @param newSize The length to make the new buffer
+         * @note This is only supported by host visible buffers. This does not support uploading to a private buffer. Reallocation is
+         * skipped if the new buffer size is less than or equal to the current buffer size.
+         */
         void ReallocateMemory(uint32 newSize) override;
+
+        /**
+         * @brief Maps a pointer to the address of this buffer
+         * @param cpuPtr The address of where the buffer address should be stored
+         */
         void MapPointer(void** cpuPtr) override;
+
+        /**
+         * @brief Unmaps a pointer from this buffer
+         * @note This does nothing on the Metal implementation
+         */
         void UnmapPointer() override;
+
+        /**
+         * @brief Copies data to this buffer
+         * @param data The address of the data to copy to the buffer
+         * @param size The size of the data to copy to this buffer
+         * @note This is only supported by host visible buffers. This does not support uploading to a private buffer
+         */
         void CopyDataToBuffer(void* data, uint32 size) override;
 
+        /**
+         * @brief Gets the native handle of the buffer
+         * @return The native handle of the buffer (MTL::Buffer*)
+        */
         void* GetNativeHandle() override;
+
+        /**
+         * @brief Copies data from a staging buffer to this buffer using a blit command
+         * @param device The graphics device
+         * @param stagingBuffer The origin buffer to copy from
+         * @param size The size of the data that should be copied to this buffer
+         * @note This forces a blocking wait while the GPU transfers the data
+         */
+        void CopyFromStagingBuffer(Device& device, Buffer& stagingBuffer, uint32 size);
+
+
+        MetalBuffer(const MetalBuffer&) = delete;
+        MetalBuffer& operator=(const MetalBuffer&) = delete;
+        MetalBuffer(MetalBuffer&& other) noexcept;
+        MetalBuffer& operator=(MetalBuffer&& other) noexcept;
 
     private:
 
-        void CreateBuffer();
-        void DestroyBuffer();
+        /**
+         * @brief Creates a buffer allocated with at least the given length
+         * @param outBuffer The address to write the new buffer to
+         * @param bufferLength The length of the buffer
+         */
+        void CreateBuffer(MTL::Buffer** outBuffer, uint32 bufferLength);
+
+        /**
+         * @brief Destroys the buffer
+         * @param buffer The buffer to destroy
+         */
+        void DestroyBuffer(MTL::Buffer* buffer);
+
 
         MTL::Device* m_Device;
         MTL::Buffer* m_Buffer;
+
+        uint32 m_BufferLength;
+        uint32 m_UsedMemorySize;
+        GPUMemoryType m_MemoryType;
     };
 
 }
