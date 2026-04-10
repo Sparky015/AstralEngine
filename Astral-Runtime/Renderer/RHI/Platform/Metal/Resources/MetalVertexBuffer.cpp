92b1a37b57f95e5aa1d416d5b51db992b1d19db0
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-04-10 15:59:00 -0400

    [AE-61] Implemented MetalVertexBuffer

---
+++ Astral-Runtime/Renderer/RHI/Platform/Metal/Resources/MetalVertexBuffer.cpp
@@ -0,0 +1,104 @@
+/**
+* @file MetalVertexBuffer.cpp
+* @author Andrew Fagan
+* @date 4/6/26
+*/
+
+#include "MetalVertexBuffer.h"
+
+namespace Astral {
+
+    MetalVertexBuffer::MetalVertexBuffer(const MetalVertexBufferDesc& desc) :
+        m_BufferLayout(desc.BufferLayout),
+        m_VertexBuffer(),
+        m_DataSize(desc.DataSize)
+    {
+    }
+
+
+    MetalVertexBuffer::~MetalVertexBuffer()
+    {
+    }
+
+
+    uint32 MetalVertexBuffer::GetSize() const
+    {
+        return m_DataSize;
+    }
+
+
+    const VertexBufferLayout& MetalVertexBuffer::GetBufferLayout() const
+    {
+        return m_BufferLayout;
+    }
+
+
+    void MetalVertexBuffer::MapPointer(void** cpuPtr)
+    {
+        m_VertexBuffer.MapPointer(cpuPtr);
+    }
+
+
+    void MetalVertexBuffer::UnmapPointer()
+    {
+        m_VertexBuffer.UnmapPointer();
+    }
+
+
+    void MetalVertexBuffer::CopyDataToBuffer(void* data, uint32 size)
+    {
+        m_VertexBuffer.CopyDataToBuffer(data, size);
+    }
+
+
+    void MetalVertexBuffer::UploadToDeviceLocalBuffer(void* data, uint32 size)
+    {
+        m_VertexBuffer.UploadToDeviceLocalBuffer(data, size);
+    }
+
+
+    void MetalVertexBuffer::ChangeMemoryType(GPUMemoryType memoryType)
+    {
+        m_VertexBuffer.ChangeMemoryType(memoryType);
+    }
+
+
+    void* MetalVertexBuffer::GetNativeHandle()
+    {
+        return m_VertexBuffer.GetNativeHandle();
+    }
+
+
+    void MetalVertexBuffer::CreateVertexBuffer(const MetalVertexBufferDesc& desc)
+    {
+        if (desc.MemoryType == GPUMemoryType::DEVICE_LOCAL)
+        {
+            MetalBufferDesc vertexBufferDesc = {
+                .Device = desc.Device,
+                .Size = desc.DataSize,
+                .MemoryType = GPUMemoryType::DEVICE_LOCAL,
+            };
+
+            m_VertexBuffer = MetalBuffer{vertexBufferDesc};
+            m_VertexBuffer.UploadToDeviceLocalBuffer(desc.VerticeData, desc.DataSize);
+        }
+        else if (desc.MemoryType == GPUMemoryType::HOST_VISIBLE)
+        {
+            MetalBufferDesc vertexBufferDesc = {
+                .Device = desc.Device,
+                .Size = desc.DataSize,
+                .MemoryType = GPUMemoryType::HOST_VISIBLE,
+            };
+
+            m_VertexBuffer = MetalBuffer{vertexBufferDesc};
+            m_VertexBuffer.CopyDataToBuffer(desc.VerticeData, desc.DataSize);
+        }
+        else
+        {
+            AE_ERROR("[VulkanVertexBuffer::CreateVertexBuffer] Memory type not implemented!")
+        }
+    }
+
+}
+
+
---
+++ Astral-Runtime/Renderer/RHI/Platform/Metal/Resources/MetalVertexBuffer.h
@@ -0,0 +1,105 @@
+/**
+* @file MetalVertexBuffer.h
+* @author Andrew Fagan
+* @date 4/6/26
+*/
+
+#pragma once
+
+#include "MetalBuffer.h"
+#include "Renderer/RHI/Resources/VertexBuffer.h"
+
+#include "Metal/MTLDevice.hpp"
+
+namespace Astral {
+
+    /**
+     * @brief A description of how to build a Metal vertex buffer
+     */
+    struct MetalVertexBufferDesc
+    {
+        MTL::Device* Device;
+        void* VerticeData;
+        uint32 DataSize;
+        VertexBufferLayout& BufferLayout;
+        GPUMemoryType MemoryType;
+    };
+
+    /**
+     * @brief A wrapper around a Metal buffer supporting vertex buffer layouts and extra convenience functions
+     */
+    class MetalVertexBuffer : public VertexBuffer
+    {
+    public:
+        explicit MetalVertexBuffer(const MetalVertexBufferDesc& desc);
+        ~MetalVertexBuffer() override;
+
+        /**
+         * @brief Gets the size of the vertex data initially written to this buffer
+         * @return The size of the vertex data initially written to this buffer
+         */
+        uint32 GetSize() const override;
+
+        /**
+         * @brief Gets the layout of the vertex data in this buffer
+         * @return The layout of the vertex data in this buffer
+         */
+        const VertexBufferLayout& GetBufferLayout() const override;
+
+        /**
+         * @brief Maps a pointer to the address of this buffer
+         * @param cpuPtr The address of where the buffer address should be stored
+         * @warning Use @ref CopyDataToBuffer to do write operations for accurate memory usage tracking of the buffer
+         */
+        void MapPointer(void** cpuPtr) override;
+
+        /**
+         * @brief Unmaps a pointer from this buffer
+         * @note This does nothing on the Metal implementation
+         */
+        void UnmapPointer() override;
+
+        /**
+         * @brief Copies data to this buffer
+         * @param data The address of the data to copy to the buffer
+         * @param size The size of the data to copy to this buffer
+         * @note This is only supported by host visible buffers. This does not support uploading to a private buffer.
+         */
+        void CopyDataToBuffer(void* data, uint32 size) override;
+
+        /**
+        * @brief Uploads the given data to this buffer
+        * @param data The address of the data to copy to the buffer
+        * @param size The size of the data to upload to this buffer
+        * @note This is only supported by private buffers. This does not support copying to a shared buffer.
+        * @warning This forces a blocking wait while the GPU transfers the data.
+        */
+        void UploadToDeviceLocalBuffer(void* data, uint32 size) override;
+
+        /**
+         * @brief Changes the memory type used by this buffer to the requested type
+         * @param memoryType The memory type to change the buffer to
+         * @warning This forces a blocking wait while the GPU transfers the data between CPU to GPU or GPU to CPU memory
+         */
+        void ChangeMemoryType(GPUMemoryType memoryType) override;
+
+        /**
+         * @brief Gets the native handle of the buffer
+         * @return The native handle of the buffer (MTL::Buffer*)
+         */
+        void* GetNativeHandle() override;
+
+    private:
+
+        /**
+         * @brief Creates a vertex buffer from a vertex buffer description
+         * @param desc The description of the vertex buffer to build
+         */
+        void CreateVertexBuffer(const MetalVertexBufferDesc& desc);
+
+        VertexBufferLayout m_BufferLayout;
+        MetalBuffer m_VertexBuffer;
+        uint32 m_DataSize;
+    };
+
+}
