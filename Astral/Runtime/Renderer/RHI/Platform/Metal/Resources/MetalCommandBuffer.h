1d0e87739dd48cb6b0a67047993ea230d7f47877
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-05-31 23:11:18 -0400

    [AE-68] Added documentation to rhi command buffer files

--- Astral/Runtime/Renderer/RHI/Platform/Metal/Resources/MetalCommandBuffer.h
+++ Astral/Runtime/Renderer/RHI/Platform/Metal/Resources/MetalCommandBuffer.h
@@ -12,11 +12,17 @@
 
 namespace Astral {
 
+    /**
+    * @brief Information to create a Metal command buffer from
+    */
     struct MetalCommandBufferDesc
     {
         MTL::Device* Device;
     };
 
+    /**
+     * @brief Wraps Metal command buffer operations
+     */
     class MetalCommandBuffer : public CommandBuffer // TODO
     {
     public:
@@ -24,7 +30,127 @@ namespace Astral {
         MetalCommandBuffer(const MetalCommandBufferDesc& computePipelineStateDesc); // TODO
         ~MetalCommandBuffer() override; // TODO
 
-        void* GetNativeHandle() override; // TODO
+        /**
+         * @brief Opens command recording for the command buffer
+         */
+        void BeginRecording() override;
+
+        /**
+         * @brief Ends command recording for the command buffer
+         */
+        void EndRecording() override;
+
+        /**
+         * @brief Resets the command buffer
+         */
+        void Reset() override;
+
+        /**
+         * @brief Binds the given pipeline
+         * @param pipeline The pipeline to bind
+         */
+        void BindPipeline(const PipelineStateHandle& pipeline) override;
+
+        /**
+         * @brief Binds the given descriptor set at the given binding
+         * @param descriptorSet The descriptor set to bind
+         * @param binding The bind slot to use
+         */
+        void BindDescriptorSet(const DescriptorSetHandle& descriptorSet, uint32 binding) override;
+
+        /**
+         * @brief Binds the given vertex buffer
+         * @param vertexBuffer The vertex buffer to bind
+         */
+        void BindVertexBuffer(const VertexBufferHandle& vertexBuffer) override;
+
+        /**
+         * @brief Binds the given index buffer
+         * @param indexBuffer The index buffer to bind
+         */
+        void BindIndexBuffer(const IndexBufferHandle& indexBuffer) override;
+
+        /**
+         * @brief Sets the viewport and scissor dimensions through the dynamic pipeline states
+         * @param dimensions The dimensions to set
+         */
+        void SetViewportAndScissor(UVec2 dimensions) override;
+
+        /**
+         * @brief Begins a render pass using the given frame buffer
+         * @param renderPassHandle The render pass being used
+         * @param frameBufferHandle The frame buffer to use with the render pass
+         */
+        void BeginRenderPass(const RenderPassHandle& renderPassHandle, const FramebufferHandle& frameBufferHandle) override;
+
+        /**
+         * @brief Indicates the start of the next subpass
+         */
+        void NextSubpass() override;
+
+        /**
+         * @brief Indicates the end of the render pass
+         */
+        void EndRenderPass() override;
+
+        /**
+         * @brief Draws the elements of the bound vertex buffer using the indices of the bound index buffer
+         * @param indexBufferHandle The index buffer currently bound
+         */
+        void DrawElementsIndexed(const IndexBufferHandle& indexBufferHandle) override;
+
+        /**
+         * @brief Draws a given number of instances of elements from the bound vertex buffer and index buffer
+         * @param indexBufferHandle The index buffer currently bound
+         * @param numberOfInstances The number of instances to draw
+         */
+        void DrawElementsInstanced(const IndexBufferHandle& indexBufferHandle, uint32 numberOfInstances) override;
+
+        /**
+         * @brief Dispatches a compute shader based on the bound compute pipeline
+         * @param groupCountX The number of groups in the X dimension
+         * @param groupCountY The number of groups in the Y dimension
+         * @param groupCountZ The number of groups in the Z dimension
+         */
+        void Dispatch(uint32 groupCountX, uint32 groupCountY, uint32 groupCountZ) override;
+
+        /**
+         * @brief Sets the push constant data into the command buffer
+         * @param data The data to copy to the push constant buffer
+         * @param sizeInBytes The number of bytes to copy from the data parameter
+         */
+        void PushConstants(void* data, uint32 sizeInBytes) override;
+
+        /**
+         * @brief Inserts a pipeline barrier into the command buffer
+         * @param pipelineBarrier The definition of the pipeline barrier to insert
+         */
+        void SetPipelineBarrier(const PipelineBarrier& pipelineBarrier) override;
+
+        /**
+         * @brief Begins a debug label
+         * @param label The label name
+         * @param color The label color shown in the graphics debugger software
+         */
+        void BeginLabel(const std::string_view& label, Vec4 color) override;
+
+        /**
+         * @brief Ends the debug label
+         */
+        void EndLabel() override;
+
+        /**
+         * @brief Inserts a debug marker into the command buffer
+         * @param label The marker name
+         * @param color The marker color shown in the graphics debugger software
+         */
+        void InsertMarker(const std::string_view& label, Vec4 color) override;
+
+        /**
+         * @brief Gets the native command buffer object
+         * @return The native command buffer object
+         */
+        void* GetNativeHandle() override;
 
     private:
 
--- Astral/Runtime/Renderer/RHI/Resources/CommandBuffer.h
+++ Astral/Runtime/Renderer/RHI/Resources/CommandBuffer.h
@@ -14,41 +14,158 @@
 
 namespace Astral {
 
+    /**
+     * @brief Wraps graphics API command buffer operations
+     */
     class CommandBuffer
     {
     public:
         virtual ~CommandBuffer() = default;
 
+        /**
+         * @brief Opens command recording for the command buffer
+         */
         virtual void BeginRecording() = 0;
+
+        /**
+         * @brief Ends command recording for the command buffer
+         */
         virtual void EndRecording() = 0;
+
+        /**
+         * @brief Resets the command buffer
+         */
         virtual void Reset() = 0;
 
+        /**
+         * @brief Binds the given pipeline
+         * @param pipeline The pipeline to bind
+         */
         virtual void BindPipeline(const PipelineStateHandle& pipeline) = 0;
+
+        /**
+         * @brief Binds the given descriptor set at the given binding
+         * @param descriptorSet The descriptor set to bind
+         * @param binding The bind slot to use
+         */
         virtual void BindDescriptorSet(const DescriptorSetHandle& descriptorSet, uint32 binding) = 0;
+
+        /**
+         * @brief Binds the given vertex buffer
+         * @param vertexBuffer The vertex buffer to bind
+         */
         virtual void BindVertexBuffer(const VertexBufferHandle& vertexBuffer) = 0;
+
+        /**
+         * @brief Binds the given index buffer
+         * @param indexBuffer The index buffer to bind
+         */
         virtual void BindIndexBuffer(const IndexBufferHandle& indexBuffer) = 0;
 
+        /**
+         * @brief Sets the viewport and scissor dimensions through the dynamic pipeline states
+         * @param dimensions The dimensions to set
+         */
         virtual void SetViewportAndScissor(UVec2 dimensions) = 0;
 
+        /**
+         * @brief Begins a render pass using the given frame buffer
+         * @param renderPassHandle The render pass being used
+         * @param frameBufferHandle The frame buffer to use with the render pass
+         */
         virtual void BeginRenderPass(const RenderPassHandle& renderPassHandle, const FramebufferHandle& frameBufferHandle) = 0;
+
+        /**
+         * @brief Indicates the start of the next subpass
+         */
         virtual void NextSubpass() = 0;
+
+        /**
+         * @brief Indicates the end of the render pass
+         */
         virtual void EndRenderPass() = 0;
 
+        /**
+         * @brief Draws the elements of the bound vertex buffer using the indices of the bound index buffer
+         * @param indexBufferHandle The index buffer currently bound
+         */
         virtual void DrawElementsIndexed(const IndexBufferHandle& indexBufferHandle) = 0;
+
+        /**
+         * @brief Draws a given number of instances of elements from the bound vertex buffer and index buffer
+         * @param indexBufferHandle The index buffer currently bound
+         * @param numberOfInstances The number of instances to draw
+         */
         virtual void DrawElementsInstanced(const IndexBufferHandle& indexBufferHandle, uint32 numberOfInstances) = 0;
+
+        /**
+         * @brief Dispatches a compute shader based on the bound compute pipeline
+         * @param groupCountX The number of groups in the X dimension
+         * @param groupCountY The number of groups in the Y dimension
+         * @param groupCountZ The number of groups in the Z dimension
+         */
         virtual void Dispatch(uint32 groupCountX, uint32 groupCountY, uint32 groupCountZ) = 0;
+
+        /**
+         * @brief Sets the push constant data into the command buffer
+         * @param data The data to copy to the push constant buffer
+         * @param sizeInBytes The number of bytes to copy from the data parameter
+         */
         virtual void PushConstants(void* data, uint32 sizeInBytes) = 0;
+
+        /**
+         * @brief Inserts a pipeline barrier into the command buffer
+         * @param pipelineBarrier The definition of the pipeline barrier to insert
+         */
         virtual void SetPipelineBarrier(const PipelineBarrier& pipelineBarrier) = 0;
 
+        /**
+         * @brief Begins a debug label
+         * @param label The label name
+         * @param color The label color shown in the graphics debugger software
+         */
         virtual void BeginLabel(const std::string_view& label, Vec4 color) = 0;
+
+        /**
+         * @brief Ends the debug label
+         */
         virtual void EndLabel() = 0;
+
+        /**
+         * @brief Inserts a debug marker into the command buffer
+         * @param label The marker name
+         * @param color The marker color shown in the graphics debugger software
+         */
         virtual void InsertMarker(const std::string_view& label, Vec4 color) = 0;
 
+        /**
+         * @brief Gets the native command buffer object
+         * @return The native command buffer object
+         */
         virtual void* GetNativeHandle() = 0;
 
+        /**
+         * @brief Checks if this command buffer is valid
+         * @return True if this command buffer is valid, false otherwise
+         */
         bool IsValid() { return m_IsValid; }
+
+        /**
+         * @brief Checks if this command buffer is in the recording mode
+         * @return True if this command buffer is in the recording mode, false otherwise
+         */
         bool IsRecording() { return m_State == State::RECORDING; }
+
+        /**
+         * @brief Checks if this command buffer has finished recording
+         * @return True if this command buffer has finished recording, false otherwise
+         */
         bool IsRecorded() { return m_State == State::RECORDED; }
+
+        /**
+         * @brief Checks if this command buffer is empty
+         * @return True if this command buffer is empty, false otherwise
+         */
         bool IsEmpty() { return m_State == State::EMPTY; }
 
     protected:
