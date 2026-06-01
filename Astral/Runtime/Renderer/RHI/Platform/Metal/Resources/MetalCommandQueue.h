ca4495490750a7c4444690b2f716e5ca8431720b
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-05-31 22:27:41 -0400

    [AE-68] Added documentation to rhi command queue files

--- Astral/Runtime/Renderer/RHI/Platform/Metal/Resources/MetalCommandQueue.h
+++ Astral/Runtime/Renderer/RHI/Platform/Metal/Resources/MetalCommandQueue.h
@@ -12,11 +12,17 @@
 
 namespace Astral {
 
+    /**
+    * @brief Information to create a Metal command queue from
+    */
     struct MetalCommandQueueDesc
     {
         MTL::Device* Device;
     };
 
+    /**
+     * @brief Wraps Metal command queue operations
+     */
     class MetalCommandQueue : public CommandQueue // TODO
     {
     public:
@@ -24,7 +30,35 @@ namespace Astral {
         MetalCommandQueue(const MetalCommandQueueDesc& commandQueueDesc); // TODO
         ~MetalCommandQueue() override; // TODO
 
-        void* GetNativeHandle() override; // TODO
+        /**
+         * @brief Submits a command buffer to a command queue
+         * @param commandBufferHandle The command buffer to submit to the command queue
+         * @param renderTargetHandle The render target with a fence and semaphores to submit with the command buffer for the frame
+         */
+        void Submit(CommandBufferHandle commandBufferHandle, RenderTargetHandle renderTargetHandle) override;
+
+        /**
+         * @brief Submits a command buffer to a command queue without a fence or semaphore to signal
+         * @param commandBufferHandle The command buffer to submit to the command queue
+         */
+        void SubmitSync(CommandBufferHandle commandBufferHandle) override;
+
+        /**
+         * @brief Presents a render target to the screen
+         * @param renderTarget The render target to present to the screen
+         */
+        void Present(RenderTargetHandle renderTarget) override;
+
+        /**
+         * @brief Blocks the current thread until the command queue becomes idle
+         */
+        void WaitIdle() override;
+
+        /**
+         * @brief Gets the native handle of the command queue
+         * @return The native handle of the command queue
+         */
+        void* GetNativeHandle() override;
 
     private:
 
