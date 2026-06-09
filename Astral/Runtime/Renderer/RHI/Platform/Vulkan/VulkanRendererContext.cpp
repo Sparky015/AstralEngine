f616f1c26d73a159807ed02ab51806b9e2f4f64f
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-06-09 13:50:23 -0400

    [AE-67] Refactored the ImGui vulkan backend to use dynamic rendering instead of the old render pass/framebuffer objects

--- Astral/Runtime/Renderer/RHI/Platform/Vulkan/VulkanRendererContext.cpp
+++ Astral/Runtime/Renderer/RHI/Platform/Vulkan/VulkanRendererContext.cpp
@@ -11,6 +11,7 @@
 #include "Core/Utilities/Error.h"
 #include "Core/Utilities/Asserts.h"
 #include "Core/Utilities/Loggers.h"
+#include "Common/VkEnumConversions.h"
 
 #ifdef ASTRAL_VULKAN_AVAILABLE
 #endif
@@ -199,9 +200,61 @@ namespace Astral {
     }
 
 
+    static void PopulateVkPipelineRenderingCreateInfoHelper(const RenderPassHandle& renderPassHandle, VkPipelineRenderingCreateInfo* outRenderingCreateInfo, std::vector<VkFormat>& outColorAttachmentFormats)
+    {
+        if (outRenderingCreateInfo == nullptr) { return; }
+
+         *outRenderingCreateInfo = {
+            .sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO,
+            .pNext = nullptr,
+            .viewMask = 0,
+            .colorAttachmentCount = 0,
+            .pColorAttachmentFormats = nullptr,
+            .depthAttachmentFormat = VK_FORMAT_UNDEFINED,
+            .stencilAttachmentFormat = VK_FORMAT_UNDEFINED,
+        };
+
+
+        // ==== Populating the color attachments format fields ========================================================
+
+        const std::vector<AttachmentReference>& colorAttachmentReferences = renderPassHandle->GetColorAttachmentReferences();
+
+        for (int i = 0; i < colorAttachmentReferences.size(); i++)
+        {
+            const AttachmentReference& colorAttachmentReference = colorAttachmentReferences[i];
+            AttachmentDescription colorAttachmentDescription = renderPassHandle->GetAttachmentDescription(colorAttachmentReference.AttachmentIndex);
+            VkFormat vkAttachmentFormat = ConvertImageFormatToVkFormat(colorAttachmentDescription.Format);
+            outColorAttachmentFormats.push_back(vkAttachmentFormat);
+        }
+
+        outRenderingCreateInfo->colorAttachmentCount = outColorAttachmentFormats.size();
+        outRenderingCreateInfo->pColorAttachmentFormats = outColorAttachmentFormats.data();
+
+        // ==== Populating the depth/stencil attachment format fields ========================================================
+
+        AttachmentReference depthStencilAttachmentReference = renderPassHandle->GetDepthStencilAttachmentReference();
+
+        if (depthStencilAttachmentReference.AttachmentIndex != NullAttachmentIndex)
+        {
+            // Depth stencil attachment exists
+
+            AttachmentDescription depthStencilAttachmentDescription = renderPassHandle->GetAttachmentDescription(depthStencilAttachmentReference.AttachmentIndex);
+            VkFormat vkDepthStencilAttachmentFormat = ConvertImageFormatToVkFormat(depthStencilAttachmentDescription.Format);
+            outRenderingCreateInfo->depthAttachmentFormat = vkDepthStencilAttachmentFormat;
+
+            if (IsStencilFormat(depthStencilAttachmentDescription.Format))
+            {
+                outRenderingCreateInfo->stencilAttachmentFormat = vkDepthStencilAttachmentFormat;
+            }
+        }
+    }
+
+
     void VulkanRenderingContext::InitImGuiForAPIBackend(RenderPassHandle renderPassHandle)
     {
-        VkRenderPass renderPass = (VkRenderPass)renderPassHandle->GetNativeHandle();
+        VkPipelineRenderingCreateInfo pipelineRenderingCreateInfo{};
+        std::vector<VkFormat> colorAttachmentFormats; // Temp var to hold color attachment formats for the lifetime of the pipelineRenderingCreateInfo variable (so that the memory does not get freed before the pipelineRenderingCreateInfo is used)
+        PopulateVkPipelineRenderingCreateInfoHelper(renderPassHandle, &pipelineRenderingCreateInfo, colorAttachmentFormats);
 
         ImGui_ImplVulkan_InitInfo initInfo =
         {
@@ -210,12 +263,14 @@ namespace Astral {
             .Device = (VkDevice)m_Device->GetNativeHandle(),
             .QueueFamily = m_QueueFamilyIndex,
             .Queue = (VkQueue)m_Device->GetPrimaryCommandQueue()->GetNativeHandle(),
-            .RenderPass = renderPass,
+            .RenderPass = nullptr,
             .MinImageCount = m_Device->GetSwapchain().GetNumberOfImages(),
             .ImageCount = m_Device->GetSwapchain().GetNumberOfImages(),
             .MSAASamples = VK_SAMPLE_COUNT_1_BIT,
             .Subpass = renderPassHandle->GetNumberOfSubpasses() - 1,
             .DescriptorPoolSize = 9,
+            .UseDynamicRendering = true,
+            .PipelineRenderingCreateInfo = pipelineRenderingCreateInfo,
             .MinAllocationSize = 1024 * 1024,
         };
 
