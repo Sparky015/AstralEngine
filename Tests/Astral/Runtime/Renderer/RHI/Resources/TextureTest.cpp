b3795c90016f4e451d02aad32f93aa25d6980ef5
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-06-26 22:34:54 -0400

    Extended texture RHI unit tests to test from combinations of test values

--- Tests/Astral/Runtime/Renderer/RHI/Resources/TextureTest.cpp
+++ Tests/Astral/Runtime/Renderer/RHI/Resources/TextureTest.cpp
@@ -12,7 +12,7 @@
 
 namespace Astral {
 
-    class TextureTests : public ::testing::Test
+    class TextureTests : public ::testing::TestWithParam<std::tuple<ImageFormat, Vec2, uint32, uint32, SamplerFilter, SamplerAddressMode>>
     {
     public:
 
@@ -23,148 +23,180 @@ namespace Astral {
 
     };
 
-    TEST_F(TextureTests, Contructor_CorrectlyCreatesTextureWithoutError)
+    TEST_P(TextureTests, Contructor_CorrectlyCreatesTextureWithoutError)
     {
+        auto& [imageFormat, dimensions, layerCount, mipMapCount, samplerFilter, samplerAddressMode] = GetParam();
+
         TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R8G8B8A8_UNORM,
+            .Format = imageFormat,
             .Layout = ImageLayout::GENERAL,
             .UsageFlags = IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-            .Dimensions = Vec2(20, 20),
+            .Dimensions = dimensions,
             .ImageData = nullptr,
-            .LayerCount = 1,
-            .MipMapCount = 1,
+            .LayerCount = layerCount,
+            .MipMapCount = mipMapCount,
+            .GenerateMipMaps = false,
+            .SamplerFilter = samplerFilter,
+            .SamplerAddressMode = samplerAddressMode,
         };
         TextureHandle testTexture = RendererAPI::GetDevice().CreateTexture(testTextureCreateInfo);
 
         ASSERT_NE(testTexture, nullptr);
         ASSERT_NE(testTexture->GetNativeImage(), nullptr);
         ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
+        ASSERT_NE(testTexture->GetNativeSampler(), nullptr);
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
 
-    TEST_F(TextureTests, Texture3D_Contructor_CorrectlyCreatesTextureWithoutError)
+    TEST_P(TextureTests, Texture3D_Contructor_CorrectlyCreatesTextureWithoutError)
     {
+        auto& [imageFormat, dimensions, layerCount, mipMapCount, samplerFilter, samplerAddressMode] = GetParam();
 
         TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R8G8B8A8_UNORM,
+            .Format = imageFormat,
             .Layout = ImageLayout::COLOR_ATTACHMENT_OPTIMAL,
             .UsageFlags = IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-            .Dimensions = Vec2(20, 20),
+            .Dimensions = dimensions,
             .ImageData = nullptr,
-            .LayerCount = 1,
-            .MipMapCount = 1,
+            .LayerCount = layerCount,
+            .MipMapCount = mipMapCount,
+            .GenerateMipMaps = false,
+            .SamplerFilter = samplerFilter,
+            .SamplerAddressMode = samplerAddressMode,
         };
+
+        if (dimensions.x > 512)
+        {
+            testTextureCreateInfo.Dimensions = Vec2(512, 512);
+        }
+
         TextureHandle testTexture = RendererAPI::GetDevice().Create3DTexture(testTextureCreateInfo);
 
         ASSERT_NE(testTexture, nullptr);
         ASSERT_NE(testTexture->GetNativeImage(), nullptr);
         ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
+        ASSERT_NE(testTexture->GetNativeSampler(), nullptr);
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
 
-    TEST_F(TextureTests, Texture2DArray_Contructor_CorrectlyCreatesTextureWithoutError)
+    TEST_P(TextureTests, Texture2DArray_Contructor_CorrectlyCreatesTextureWithoutError)
     {
+        auto& [imageFormat, dimensions, layerCount, mipMapCount, samplerFilter, samplerAddressMode] = GetParam();
+
         TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R16G16B16A16_SFLOAT,
+            .Format = imageFormat,
             .Layout = ImageLayout::GENERAL,
             .UsageFlags = IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-            .Dimensions = Vec2(40, 40),
+            .Dimensions = dimensions,
             .ImageData = nullptr,
-            .LayerCount = 10,
-            .MipMapCount = 1,
+            .LayerCount = layerCount,
+            .MipMapCount = mipMapCount,
+            .GenerateMipMaps = false,
+            .SamplerFilter = samplerFilter,
+            .SamplerAddressMode = samplerAddressMode,
         };
         TextureHandle testTexture = RendererAPI::GetDevice().Create2DTextureArray(testTextureCreateInfo);
 
         ASSERT_NE(testTexture, nullptr);
         ASSERT_NE(testTexture->GetNativeImage(), nullptr);
         ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
+        ASSERT_NE(testTexture->GetNativeSampler(), nullptr);
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
 
-    TEST_F(TextureTests, TextureCubemap_Contructor_CorrectlyCreatesTextureWithoutError)
+    TEST_P(TextureTests, TextureCubemap_Contructor_CorrectlyCreatesTextureWithoutError)
     {
+        auto& [imageFormat, dimensions, layerCount, mipMapCount, samplerFilter, samplerAddressMode] = GetParam();
+
         TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R8G8B8A8_UNORM,
+            .Format = imageFormat,
             .Layout = ImageLayout::GENERAL,
             .UsageFlags = IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-            .Dimensions = Vec2(20, 20),
+            .Dimensions = dimensions,
             .ImageData = nullptr,
-            .LayerCount = 1,
-            .MipMapCount = 1,
+            .LayerCount = layerCount,
+            .MipMapCount = mipMapCount,
+            .GenerateMipMaps = false,
+            .SamplerFilter = samplerFilter,
+            .SamplerAddressMode = samplerAddressMode,
         };
         TextureHandle testTexture = RendererAPI::GetDevice().CreateCubemap(testTextureCreateInfo);
 
         ASSERT_NE(testTexture, nullptr);
         ASSERT_NE(testTexture->GetNativeImage(), nullptr);
         ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
+        ASSERT_NE(testTexture->GetNativeSampler(), nullptr);
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
 
-    TEST_F(TextureTests, Texture1D_Contructor_CorrectlyCreatesTextureWithoutError)
+    TEST_P(TextureTests, Texture1D_Contructor_CorrectlyCreatesTextureWithoutError)
     {
+        auto& [imageFormat, dimensions, layerCount, mipMapCount, samplerFilter, samplerAddressMode] = GetParam();
+
         TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R8_UNORM,
+            .Format = imageFormat,
             .Layout = ImageLayout::SHADER_READ_ONLY_OPTIMAL,
             .UsageFlags = IMAGE_USAGE_SAMPLED_BIT,
-            .Dimensions = Vec2(50, 0),
+            .Dimensions = dimensions,
             .ImageData = nullptr,
-            .LayerCount = 1,
-            .MipMapCount = 1,
+            .LayerCount = layerCount,
+            .MipMapCount = mipMapCount,
+            .GenerateMipMaps = false,
+            .SamplerFilter = samplerFilter,
+            .SamplerAddressMode = samplerAddressMode,
         };
         TextureHandle testTexture = RendererAPI::GetDevice().Create1DTexture(testTextureCreateInfo);
 
         ASSERT_NE(testTexture, nullptr);
         ASSERT_NE(testTexture->GetNativeImage(), nullptr);
         ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
+        ASSERT_NE(testTexture->GetNativeSampler(), nullptr);
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
 
-    TEST_F(TextureTests, ContructorWithMipMaps_CorrectlyCreatesTextureWithoutError)
+    TEST_P(TextureTests, ContructorWithMipMaps_CorrectlyCreatesTextureWithoutError)
     {
+        auto& [imageFormat, dimensions, layerCount, mipMapCount, samplerFilter, samplerAddressMode] = GetParam();
+
         TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R8_UNORM,
+            .Format = imageFormat,
             .Layout = ImageLayout::SHADER_READ_ONLY_OPTIMAL,
             .UsageFlags = IMAGE_USAGE_SAMPLED_BIT,
-            .Dimensions = Vec2(1024, 1024),
+            .Dimensions = dimensions,
             .ImageData = nullptr,
-            .LayerCount = 1,
-            .MipMapCount = Texture::CalculateMipMapLevels(1024, 1024),
-            .GenerateMipMaps = true
+            .LayerCount = layerCount,
+            .MipMapCount = Texture::CalculateMipMapLevels(dimensions.x, dimensions.y),
+            .GenerateMipMaps = true,
+            .SamplerFilter = samplerFilter,
+            .SamplerAddressMode = samplerAddressMode,
         };
         TextureHandle testTexture = RendererAPI::GetDevice().CreateTexture(testTextureCreateInfo);
 
         ASSERT_NE(testTexture, nullptr);
         ASSERT_NE(testTexture->GetNativeImage(), nullptr);
         ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
+        ASSERT_NE(testTexture->GetNativeSampler(), nullptr);
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
 
-    TEST_F(TextureTests, ContructorWithEditedSampler_CorrectlyCreatesTextureWithoutError)
-    {
-        TextureCreateInfo testTextureCreateInfo = {
-            .Format = ImageFormat::R8_UNORM,
-            .Layout = ImageLayout::SHADER_READ_ONLY_OPTIMAL,
-            .UsageFlags = IMAGE_USAGE_SAMPLED_BIT,
-            .Dimensions = Vec2(1024, 1024),
-            .ImageData = nullptr,
-            .LayerCount = 1,
-            .MipMapCount = 1,
-            .SamplerFilter = SamplerFilter::NEAREST,
-            .SamplerAddressMode = SamplerAddressMode::CLAMP_TO_EDGE,
-        };
-        TextureHandle testTexture = RendererAPI::GetDevice().CreateTexture(testTextureCreateInfo);
-
-        ASSERT_NE(testTexture, nullptr);
-        ASSERT_NE(testTexture->GetNativeImage(), nullptr);
-        ASSERT_NE(testTexture->GetNativeImageView(), nullptr);
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
-
+    INSTANTIATE_TEST_SUITE_P(
+        TextureTestsParams,
+        TextureTests,
+        ::testing::Combine(
+            ::testing::Values(ImageFormat::R8_UNORM, ImageFormat::R8G8B8A8_UNORM, ImageFormat::R16G16B16A16_SFLOAT), // Image Format
+            ::testing::Values(Vec2(1, 1), Vec2(20, 20), Vec2(512, 512), Vec2(1024, 1024)),                           // Image Dimensions
+            ::testing::Values(1, 2, 5),                                                                              // Layer Count
+            ::testing::Values(1, 3, 5),                                                                              // Mip Map Count
+            ::testing::Values(SamplerFilter::NEAREST, SamplerFilter::LINEAR),                                        // Sampler Filter
+            ::testing::Values(SamplerAddressMode::REPEAT, SamplerAddressMode::CLAMP_TO_EDGE,                         // Sampler Address Mode
+                              SamplerAddressMode::CLAMP_TO_BORDER)
+        )
+    );
 
 }
