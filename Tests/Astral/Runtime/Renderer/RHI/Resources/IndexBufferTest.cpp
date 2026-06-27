651fb5de54ba186952fcf267c49ab3c5eaff1e0d
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-06-26 23:14:28 -0400

    Expanded the index and vertex buffer unit tests to use combinational parameters in unit tests

--- Tests/Astral/Runtime/Renderer/RHI/Resources/IndexBufferTest.cpp
+++ Tests/Astral/Runtime/Renderer/RHI/Resources/IndexBufferTest.cpp
@@ -11,7 +11,7 @@
 
 namespace Astral {
 
-    class IndexBufferTests : public ::testing::Test
+    class IndexBufferTests : public ::testing::TestWithParam<std::tuple<std::vector<uint32>, GPUMemoryType>>
     {
     public:
 
@@ -22,97 +22,61 @@ namespace Astral {
 
     };
 
-    TEST_F(IndexBufferTests, Constructor_CorrectlyCreatesBufferWithDataPopulated)
+    TEST_P(IndexBufferTests, Constructor_CorrectlyCreatesBufferWithDataPopulated)
     {
         {
-            std::array<uint32, 3> indices = {0, 1, 2};
-            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer(indices.data(), sizeof(indices), GPUMemoryType::DEVICE_LOCAL);
+            auto& [indexData, gpuMemoryType] = GetParam();
+            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer((uint32*)indexData.data(), indexData.size() * sizeof(uint32), gpuMemoryType);
 
             ASSERT_NE(indexBuffer, nullptr);
             ASSERT_NE(indexBuffer->GetNativeHandle(), nullptr);
-            ASSERT_EQ(indexBuffer->GetCount(), 3);
+            ASSERT_EQ(indexBuffer->GetCount(), indexData.size());
         }
 
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
-    TEST_F(IndexBufferTests, Constructor_2_CorrectlyCreatesBufferWithDataPopulated)
+
+    TEST_P(IndexBufferTests, Constructor_ConstructsBufferAndReadsBackDataCorrectly)
     {
         {
-            std::array<uint32, 7> indices = {0, 1, 2, 1, 2, 5, 2};
-            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer(indices.data(), sizeof(indices), GPUMemoryType::HOST_VISIBLE);
+            auto& [indexData, gpuMemoryType] = GetParam();
+            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer((uint32*)indexData.data(), indexData.size() * sizeof(uint32), gpuMemoryType);
 
             ASSERT_NE(indexBuffer, nullptr);
             ASSERT_NE(indexBuffer->GetNativeHandle(), nullptr);
-            ASSERT_EQ(indexBuffer->GetCount(), 7);
-        }
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
+            ASSERT_EQ(indexBuffer->GetCount(), indexData.size());
 
-    TEST_F(IndexBufferTests, ConstructorUsingHostVisibleMemory_CorrectlyCreatesBufferWithDataPopulated)
-    {
-        {
-            std::array<uint32, 4> indices = {0, 1, 2, 1};
-            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer(indices.data(), sizeof(indices), GPUMemoryType::HOST_VISIBLE);
-
-            ASSERT_NE(indexBuffer, nullptr);
-            ASSERT_NE(indexBuffer->GetNativeHandle(), nullptr);
-            ASSERT_EQ(indexBuffer->GetCount(), 4);
+            if (gpuMemoryType == GPUMemoryType::DEVICE_LOCAL)
+            {
+                indexBuffer->ChangeMemoryType(GPUMemoryType::HOST_VISIBLE);
+            }
 
             void* bufferIndices;
             indexBuffer->MapPointer(&bufferIndices);
-            ASSERT_EQ(memcmp(indices.data(), bufferIndices, sizeof(indices)), 0);
+            ASSERT_EQ(memcmp(indexData.data(), bufferIndices, indexData.size() * sizeof(uint32)), 0);
             indexBuffer->UnmapPointer();
         }
 
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
-    TEST_F(IndexBufferTests, ConstructorUsingHostVisibleMemory_2_CorrectlyCreatesBufferWithDataPopulated)
-    {
-        {
-            std::array<uint32, 6> indices = {0, 1, 2, 1, 8, 4};
-            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer(indices.data(), sizeof(indices), GPUMemoryType::HOST_VISIBLE);
 
-            ASSERT_NE(indexBuffer, nullptr);
-            ASSERT_NE(indexBuffer->GetNativeHandle(), nullptr);
-            ASSERT_EQ(indexBuffer->GetCount(), 6);
-
-            void* bufferIndices;
-            indexBuffer->MapPointer(&bufferIndices);
-            ASSERT_EQ(memcmp(indices.data(), bufferIndices, sizeof(indices)), 0);
-            indexBuffer->UnmapPointer();
-        }
-
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
-
-    TEST_F(IndexBufferTests, GetCount_CorrectlyReturnsTheCountOfIndices)
-    {
-        {
-            std::array<uint32, 6> indices = {0, 1, 2, 1, 2, 3};
-            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer(indices.data(), sizeof(indices), GPUMemoryType::DEVICE_LOCAL);
-
-            ASSERT_NE(indexBuffer, nullptr);
-            ASSERT_NE(indexBuffer->GetNativeHandle(), nullptr);
-            ASSERT_EQ(indexBuffer->GetCount(), 6);
-        }
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
-
-    TEST_F(IndexBufferTests, GetCount_2_CorrectlyReturnsTheCountOfIndices)
-    {
-        {
-            std::array<uint32, 10> indices = {0, 1, 2, 1, 2, 3, 5, 4, 1, 0};
-            IndexBufferHandle indexBuffer = RendererAPI::GetDevice().CreateIndexBuffer(indices.data(), sizeof(indices), GPUMemoryType::HOST_VISIBLE);
-
-            ASSERT_NE(indexBuffer, nullptr);
-            ASSERT_NE(indexBuffer->GetNativeHandle(), nullptr);
-            ASSERT_EQ(indexBuffer->GetCount(), 10);
-        }
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
-
-
+    INSTANTIATE_TEST_SUITE_P(
+        IndexBufferTestsParams,
+        IndexBufferTests,
+        ::testing::Combine(
+            ::testing::Values(
+                std::vector<uint32>{0, 1, 2, 4, 5, 6, 3, 6, 1, 6, 8, 9, 1, 6, 7},
+                std::vector<uint32>{1, 3, 4, 12, 3344, 15125, 12512, 111, 63212, 6666, 121, 0},
+                std::vector<uint32>{1, 3, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 12, 34, 1, 46788, 12512, 1252, 12521, 25125, 12512},
+                std::vector<uint32>{112, 4241, 123124, 566, 333, 33, 3, 1231, 568, 3785, 4125, 1352, 23576, 12451, 2225, 22,
+                                    44212, 124332, 123123, 123, 12312, 25125, 563, 35135, 235231, 12435, 5356, 345235, 2352,
+                                    435235, 12412, 12452, 6785, 87654, 124, 242, 779, 12, 52, 1, 5677, 3124, 1253, 6325, 43},
+                std::vector<uint32>{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+            ),
+            ::testing::Values(GPUMemoryType::HOST_VISIBLE, GPUMemoryType::DEVICE_LOCAL)
+        )
+    );
 
 }
--- Tests/Astral/Runtime/Renderer/RHI/Resources/VertexBufferTest.cpp
+++ Tests/Astral/Runtime/Renderer/RHI/Resources/VertexBufferTest.cpp
@@ -11,7 +11,7 @@
 
 namespace Astral {
 
-    class VertexBufferTests : public ::testing::Test
+    class VertexBufferTests : public ::testing::TestWithParam<std::tuple<std::pair<std::vector<float>, VertexBufferLayout>, GPUMemoryType>>
     {
     public:
 
@@ -22,12 +22,13 @@ namespace Astral {
 
     };
 
-    TEST_F(VertexBufferTests, Constructor_CorrectlyCreatesBufferWithDataPopulated)
+    TEST_P(VertexBufferTests, Constructor_CorrectlyCreatesBufferWithDataPopulated)
     {
         {
-            VertexBufferLayout bufferLayout = {{Float, "TestFloat"}};
-            std::array<uint32, 3> vertexData = {0, 1, 2};
-            VertexBufferHandle vertexBuffer = RendererAPI::GetDevice().CreateVertexBuffer(vertexData.data(), sizeof(vertexData), bufferLayout, GPUMemoryType::DEVICE_LOCAL);
+            auto& [dataBufferLayoutPair, memoryType] = GetParam();
+            VertexBufferLayout bufferLayout = dataBufferLayoutPair.second;
+            std::vector<float> vertexData = dataBufferLayoutPair.first;
+            VertexBufferHandle vertexBuffer = RendererAPI::GetDevice().CreateVertexBuffer(vertexData.data(), sizeof(vertexData), bufferLayout, memoryType);
 
             ASSERT_NE(vertexBuffer, nullptr);
             ASSERT_NE(vertexBuffer->GetNativeHandle(), nullptr);
@@ -35,28 +36,21 @@ namespace Astral {
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
-    TEST_F(VertexBufferTests, Constructor_2_CorrectlyCreatesBufferWithDataPopulated)
+    TEST_P(VertexBufferTests, Constructor_ConstructsBufferAndReadsBackDataCorrectly)
     {
         {
-            VertexBufferLayout bufferLayout = {{Float, "TestFloat"}, {Float2, "TestVec2"}};
-            std::array<uint32, 12> vertexData = {0, 1, 2, 4, 5, 6, 3, 6, 1, 6, 8, 9};
-            VertexBufferHandle vertexBuffer = RendererAPI::GetDevice().CreateVertexBuffer(vertexData.data(), sizeof(vertexData), bufferLayout, GPUMemoryType::HOST_VISIBLE);
+            auto& [dataBufferLayoutPair, memoryType] = GetParam();
+            VertexBufferLayout bufferLayout = dataBufferLayoutPair.second;
+            std::vector<float> vertexData = dataBufferLayoutPair.first;
+            VertexBufferHandle vertexBuffer = RendererAPI::GetDevice().CreateVertexBuffer(vertexData.data(), sizeof(vertexData), bufferLayout, memoryType);
 
             ASSERT_NE(vertexBuffer, nullptr);
             ASSERT_NE(vertexBuffer->GetNativeHandle(), nullptr);
-        }
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
 
-    TEST_F(VertexBufferTests, ConstructorUsingHostVisibleMemory_CorrectlyCreatesBufferWithDataPopulated)
-    {
-        {
-            VertexBufferLayout bufferLayout = {{Float, "TestFloat"}, {Float2, "TestVec2"}};
-            std::array<uint32, 12> vertexData = {0, 1, 2, 4, 5, 6, 3, 6, 1, 6, 8, 9};
-            VertexBufferHandle vertexBuffer = RendererAPI::GetDevice().CreateVertexBuffer(vertexData.data(), sizeof(vertexData), bufferLayout, GPUMemoryType::HOST_VISIBLE);
-
-            ASSERT_NE(vertexBuffer, nullptr);
-            ASSERT_NE(vertexBuffer->GetNativeHandle(), nullptr);
+            if (memoryType == GPUMemoryType::DEVICE_LOCAL)
+            {
+                vertexBuffer->ChangeMemoryType(GPUMemoryType::HOST_VISIBLE);
+            }
 
             void* bufferVertexData;
             vertexBuffer->MapPointer(&bufferVertexData);
@@ -67,23 +61,26 @@ namespace Astral {
         ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
     }
 
-    TEST_F(VertexBufferTests, ConstructorUsingHostVisibleMemory_2_CorrectlyCreatesBufferWithDataPopulated)
-    {
-        {
-            VertexBufferLayout bufferLayout = {{Float3, "TestVec3"}, {Float2, "TestVec2"}};
-            std::array<uint32, 15> vertexData = {0, 1, 2, 4, 5, 6, 3, 6, 1, 6, 8, 9, 1, 6, 7};
-            VertexBufferHandle vertexBuffer = RendererAPI::GetDevice().CreateVertexBuffer(vertexData.data(), sizeof(vertexData), bufferLayout, GPUMemoryType::HOST_VISIBLE);
-
-            ASSERT_NE(vertexBuffer, nullptr);
-            ASSERT_NE(vertexBuffer->GetNativeHandle(), nullptr);
-
-            void* bufferVertexData;
-            vertexBuffer->MapPointer(&bufferVertexData);
-            ASSERT_EQ(memcmp(vertexData.data(), bufferVertexData, sizeof(vertexData)), 0);
-            vertexBuffer->UnmapPointer();
-        }
-
-        ASSERT_EQ(RendererAPI::GetContext().GetNumValidationErrorsAndWarnings(), 0);
-    }
+    INSTANTIATE_TEST_SUITE_P(
+        VertexBufferTestsParams,
+        VertexBufferTests,
+        ::testing::Combine(
+            ::testing::Values(
+                std::pair<std::vector<float>, VertexBufferLayout>({0, 1, 2, 4, 5, 6, 3, 6, 1, 6, 8, 9, 1, 6, 7},
+                                                                  {{Float3, "TestVec3"}, {Float2, "TestVec2"}}),
+                std::pair<std::vector<float>, VertexBufferLayout>({1, 3, 4, 12, 3344, 15125, 12512, 111, 63212, 6666, 121, 0},
+                                                                  {{Float4, "TestVec4"}}),
+                std::pair<std::vector<float>, VertexBufferLayout>({1, 3, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 12, 34, 1, 46788, 12512, 1252, 12521, 25125, 12512},
+                                                                 {{Float4, "TestVec4"}, {Float3, "TestVec3"}}),
+                std::pair<std::vector<float>, VertexBufferLayout>({112, 4241, 123124, 566, 333, 33, 3, 1231, 568, 3785, 4125, 1352, 23576, 12451, 2225, 22,
+                                                                   44212, 124332, 123123, 123, 12312, 25125, 563, 35135, 235231, 12435, 5356, 345235, 2352,
+                                                                   435235, 12412, 12452, 6785, 87654, 124, 242, 779, 12, 52, 1, 5677, 3124, 1253, 6325, 43},
+                                                                  {{Float4, "TestVec4"}, {Float2, "TestVec2"}, {Float3, "TestVec3"}}),
+                std::pair<std::vector<float>, VertexBufferLayout>({0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                                                                 {{Float, "TestFloat"}})
+            ),
+            ::testing::Values(GPUMemoryType::HOST_VISIBLE, GPUMemoryType::DEVICE_LOCAL)
+        )
+    );
 
 }
