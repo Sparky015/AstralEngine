861ca0dad3cf5c9bc6312ae0498aee72c0003e00
Sparky015 <150553099+Sparky015@users.noreply.github.com>
2026-07-16 16:48:53 -0400

    Removed test code from new delete overrides

--- Astral/Profiler/Library/Profiler/MemoryTracking/NewDeleteOverrides.cpp
+++ Astral/Profiler/Library/Profiler/MemoryTracking/NewDeleteOverrides.cpp
@@ -16,42 +16,31 @@ static std::mutex allocatorMutex = {};
 
 void* operator new(std::size_t size)
 {
-    static Astral::TLSFAllocator allocator = Astral::TLSFAllocator(4'000'000);
-    static std::mutex allocatorMutex = {};
-
-    std::unique_lock lock (allocatorMutex);
-    return allocator.Allocate(size);
-    // void* pointer = malloc(size);
-    // if (!pointer) throw std::bad_alloc();
-    // Astral::MemoryTracker::Get().AddAllocation(pointer, size, Astral::MemoryRegion::UNKNOWN, Astral::MemoryTrackerAllocatorType::NEW_OPERATOR);
-    // return pointer;
+    void* pointer = malloc(size);
+    if (!pointer) throw std::bad_alloc();
+    Astral::MemoryTracker::Get().AddAllocation(pointer, size, Astral::MemoryRegion::UNKNOWN, Astral::MemoryTrackerAllocatorType::NEW_OPERATOR);
+    return pointer;
 }
 
 
 void* operator new[](std::size_t size)
 {
-    std::unique_lock lock (allocatorMutex);
-    return allocator.Allocate(size);
-    // void* pointer = malloc(size);
-    // if (!pointer) throw std::bad_alloc();
-    // Astral::MemoryTracker::Get().AddAllocation(pointer, size, Astral::MemoryRegion::UNKNOWN, Astral::MemoryTrackerAllocatorType::NEW_OPERATOR);
-    // return pointer;
+    void* pointer = malloc(size);
+    if (!pointer) throw std::bad_alloc();
+    Astral::MemoryTracker::Get().AddAllocation(pointer, size, Astral::MemoryRegion::UNKNOWN, Astral::MemoryTrackerAllocatorType::NEW_OPERATOR);
+    return pointer;
 }
 
 
 void operator delete(void* pointer) noexcept
 {
-    std::unique_lock lock (allocatorMutex);
-    allocator.Free(pointer);
-    // Astral::MemoryTracker::Get().RemoveAllocation(pointer);
-    // free(pointer);
+    Astral::MemoryTracker::Get().RemoveAllocation(pointer);
+    free(pointer);
 }
 
 
 void operator delete[](void* pointer) noexcept
 {
-    std::unique_lock lock (allocatorMutex);
-    allocator.Free(pointer);
-    // Astral::MemoryTracker::Get().RemoveAllocation(pointer);
-    // free(pointer);
+    Astral::MemoryTracker::Get().RemoveAllocation(pointer);
+    free(pointer);
 }
